606d9e11b24ed551ad8a769a1a8034ac7563645c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-06 12:10:37 +0300

    - Generate component 'SliderDefaultComponent'.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SliderDefaultComponent } from './slider-default/slider-default.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SliderDefaultComponent],
   imports: [
     CommonModule
   ]
---
+++ src/app/home/slider-default/slider-default.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-slider-default',
+  templateUrl: './slider-default.component.html',
+  styles: [
+  ]
+})
+export class SliderDefaultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
