63d24877b41d1a6fbd119794603e92ca568421a8
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-06 12:07:55 +0300

    - Generate module 'HomeModule'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'mak-pit-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'makamithi-frontend';
+
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    // responsiveClass: true,
+    slideBy: 1,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 1,
+        slideBy: 1,
+        nav: false,
+      },
+      991: {
+        items: 1,
+        slideBy: 1,
+        nav: false,
+      },
+      1024: {
+        items: 1,
+        slideBy: 1,
+        autoplay: true
+      }
+    },
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
