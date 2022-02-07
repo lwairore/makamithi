377dac1ece966a1c79041487dc03cb4992c95208
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 17:52:41 +0300

    - Organize application.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,36 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { TopHeaderComponent } from './header/sections/top-header/top-header.component';
-import { BurgerMenusComponent } from './common/burger-menus/burger-menus.component';
-import { SidebarComponent } from './common/sidebar/sidebar.component';
-import { SearchBarComponent } from './common/search-bar/search-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
-import { HeaderStyleTwoComponent } from './header-style-two/header-style-two.component';
-import { TopHeader2Component } from './header-style-two/sections/top-header2/top-header2.component';
-import { StickyHeaderComponent } from './common/sticky-header/sticky-header.component';
-
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    TopHeaderComponent,
-    BurgerMenusComponent,
-    SidebarComponent,
-    SearchBarComponent,
     FooterComponent,
-    HeaderStyleTwoComponent,
-    TopHeader2Component,
-    StickyHeaderComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
   ],
   exports: [
-    HeaderComponent,
     FooterComponent,
   ]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SiteBreadcrumbComponent } from './site-breadcrumb/site-breadcrumb.component';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './header/sections/top-header/top-header.component';
+import { BurgerMenusComponent } from './common/burger-menus/burger-menus.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { HeaderStyleTwoComponent } from './header-style-two/header-style-two.component';
+import { SearchBarComponent } from './common/search-bar/search-bar.component';
+import { StickyHeaderComponent } from './common/sticky-header/sticky-header.component';
+import { TopHeader2Component } from './header-style-two/sections/top-header2/top-header2.component';
 
 
 
 @NgModule({
   declarations: [
     SiteBreadcrumbComponent,
+    HeaderComponent,
+    TopHeaderComponent,
+    BurgerMenusComponent,
+    SidebarComponent,
+    SearchBarComponent,
+    HeaderStyleTwoComponent,
+    TopHeader2Component,
+    StickyHeaderComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     SiteBreadcrumbComponent,
+    HeaderComponent,
+    HeaderStyleTwoComponent,
   ]
 })
 export class SharedModule { }
