08a29740a1badb5c3a1fd7bc2b60e3faf43fd584
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 13:22:08 +0300

    - Get all references for directive 'AccordionItemDirective'.

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ContentChildren, Input, OnInit, QueryList } from '@angular/core';
+import { AccordionItemDirective } from './directives/accordion-item.directive';
 
 @Component({
   selector: 'mak-pit-accordion',
@@ -10,13 +11,16 @@ export class AccordionComponent implements OnInit {
   // A Set is used to maintain the state of currently expanded
   // accordion items. A set guarantees distinct values.
   expanded = new Set<number>();
-  
+
   /**
    * Decides if the single item will be open at once or not.
    * In collapsing mode, toggling one would collapse others
    */
 
   @Input() collapsing = true;
+
+  @ContentChildren(AccordionItemDirective)
+  items: QueryList<AccordionItemDirective>;
 
   constructor() { }
 
