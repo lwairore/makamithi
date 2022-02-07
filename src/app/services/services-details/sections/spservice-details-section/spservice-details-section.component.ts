a7c774ebedc11819751edfdffe99d8420f0ceaa5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 20:54:22 +0300

    - Generate component 'SPServiceDetailsSectionComponent'.

---
+++ src/app/services/services-details/sections/spservice-details-section/spservice-details-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-spservice-details-section',
+  templateUrl: './spservice-details-section.component.html',
+  styles: [
+  ]
+})
+export class SPServiceDetailsSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/services/services-details/services-details.module.ts
+++ src/app/services/services-details/services-details.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ServicesDetailsRoutingModule } from './services-details-routing.module';
 import { ServiceDetailsMainComponent } from './service-details-main/service-details-main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SPServiceDetailsSectionComponent } from './sections/spservice-details-section/spservice-details-section.component';
 
 
 @NgModule({
   declarations: [
     ServiceDetailsMainComponent,
+    SPServiceDetailsSectionComponent,
   ],
   imports: [
     CommonModule,
