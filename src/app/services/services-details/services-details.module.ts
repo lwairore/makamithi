aec04199e80c020be2aecc4154756e09335d7666
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:38:33 +0300

    - Improve app structure.

--- src/app/services/services-details/services-details.module.ts
+++ src/app/services/services-details/services-details.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ServicesDetailsRoutingModule } from './services-details-routing.module';
 import { ServiceDetailsMainComponent } from './service-details-main/service-details-main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { SPServiceDetailsSectionComponent } from './sections/spservice-details-section/spservice-details-section.component';
-import { SPServicePricingSectionComponent } from './sections/spservice-pricing-section/spservice-pricing-section.component';
+import { SPServiceDetailsSectionComponent } from './service-details-main/sections/spservice-details-section/spservice-details-section.component';
+import { SPServicePricingSectionComponent } from './service-details-main/sections/spservice-pricing-section/spservice-pricing-section.component';
 
 
 @NgModule({
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesComponent } from './services.component';
 import { SharedModule } from '../shared/shared.module';
-import { SPStyleOneComponent } from './sections/spstyle-one/spstyle-one.component';
-import { SPStyleTwoComponent } from './sections/spstyle-two/spstyle-two.component';
-import { SPVideoSectionComponent } from './sections/spvideo-section/spvideo-section.component';
-import { SPAboutSectionComponent } from './sections/spabout-section/spabout-section.component';
-import { SPPricingComponent } from './sections/sppricing/sppricing.component';
+import { SPStyleOneComponent } from './service-one-main/sections/spstyle-one/spstyle-one.component';
+import { SPStyleTwoComponent } from './service-one-main/sections/spstyle-two/spstyle-two.component';
+import { SPVideoSectionComponent } from './service-one-main/sections/spvideo-section/spvideo-section.component';
+import { SPAboutSectionComponent } from './service-one-main/sections/spabout-section/spabout-section.component';
+import { SPPricingComponent } from './service-one-main/sections/sppricing/sppricing.component';
 import { ServiceOneMainComponent } from './service-one-main/service-one-main.component';
 
 
