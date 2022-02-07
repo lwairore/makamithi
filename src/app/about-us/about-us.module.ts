1554d64da7cd83e38cc129b3da929b1a0ee1c23d
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 11:25:24 +0300

    - Generate component 'SiteBreadcrumbComponent'.

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutMainComponent } from './about-main/about-main.component';
+import { SiteBreadcrumbComponent } from './sections/site-breadcrumb/site-breadcrumb.component';
 
 
 @NgModule({
-  declarations: [AboutMainComponent],
+  declarations: [AboutMainComponent, SiteBreadcrumbComponent],
   imports: [
     CommonModule,
     AboutUsRoutingModule
---
+++ src/app/about-us/sections/site-breadcrumb/site-breadcrumb.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-site-breadcrumb',
+  templateUrl: './site-breadcrumb.component.html',
+  styles: [
+  ]
+})
+export class SiteBreadcrumbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
