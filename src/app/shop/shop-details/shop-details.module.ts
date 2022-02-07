307736c448dc43ae1a32ae8c33bec684e6d4e1ab
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:39:10 +0300

    - Generate module 'ShopModule'.

---
+++ src/app/shop/shop-details/shop-details.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShopDetailsRoutingModule } from './shop-details-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ShopDetailsRoutingModule
+  ]
+})
+export class ShopDetailsModule { }
