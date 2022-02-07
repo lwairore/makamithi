1aca9b63e81a1020f272107733738cc67f334e9c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:57:19 +0300

    - Import module 'SharedModule'.

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { BlogComponent } from './blog.component';
   ],
   imports: [
     CommonModule,
-    BlogRoutingModule
+    BlogRoutingModule,
+    SharedModule,
   ]
 })
 export class BlogModule { }
