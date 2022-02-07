b5fc1b4f1db0fa62ec85a278cdde65486f75f5ec
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 11:30:40 +0300

    - Generate component 'APAbout4SectionComponent'.

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutMainComponent } from './about-main/about-main.component';
 import { SiteBreadcrumbComponent } from './sections/site-breadcrumb/site-breadcrumb.component';
+import { APAbout4SectionComponent } from './sections/apabout4-section/apabout4-section.component';
 
 
 @NgModule({
-  declarations: [AboutMainComponent, SiteBreadcrumbComponent],
+  declarations: [AboutMainComponent, SiteBreadcrumbComponent, APAbout4SectionComponent],
   imports: [
     CommonModule,
     AboutUsRoutingModule
---
+++ src/app/about-us/sections/apabout4-section/apabout4-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-apabout4-section',
+  templateUrl: './apabout4-section.component.html',
+  styles: [
+  ]
+})
+export class APAbout4SectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
