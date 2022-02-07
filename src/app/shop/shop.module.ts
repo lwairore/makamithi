9e4d57a9352c784827d5ba2d8969efa0cdec100e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:16:20 +0300

    - Format module.

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ShopRoutingModule } from './shop-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    
+  ],
   imports: [
     CommonModule,
     ShopRoutingModule
