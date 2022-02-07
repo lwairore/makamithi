266a354f100de9f851c2b669625d5b790fb44672
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:45:39 +0300

    - Generate component 'SPShopDetailsSectionComponent'.

---
+++ src/app/shop/shop-details/shop-details-main/sections/spshop-details-section/spshop-details-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-spshop-details-section',
+  templateUrl: './spshop-details-section.component.html',
+  styles: [
+  ]
+})
+export class SPShopDetailsSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shop/shop-details/shop-details.module.ts
+++ src/app/shop/shop-details/shop-details.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ShopDetailsRoutingModule } from './shop-details-routing.module';
 import { ShopDetailsMainComponent } from './shop-details-main/shop-details-main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SPShopDetailsSectionComponent } from './shop-details-main/sections/spshop-details-section/spshop-details-section.component';
 
 
 @NgModule({
   declarations: [
     ShopDetailsMainComponent,
+    SPShopDetailsSectionComponent,
   ],
   imports: [
     CommonModule,
