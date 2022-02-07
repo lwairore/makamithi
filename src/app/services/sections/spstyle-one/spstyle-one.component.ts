6347bb2fd7dec5a9e50d731308967ef06051cd62
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 20:04:42 +0300

    - Generate component 'SPStyleOneComponent'.

---
+++ src/app/services/sections/spstyle-one/spstyle-one.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-spstyle-one',
+  templateUrl: './spstyle-one.component.html',
+  styles: [
+  ]
+})
+export class SPStyleOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesComponent } from './services.component';
 import { SharedModule } from '../shared/shared.module';
+import { SPStyleOneComponent } from './sections/spstyle-one/spstyle-one.component';
 
 
 @NgModule({
   declarations: [
     ServicesComponent,
+    SPStyleOneComponent,
   ],
   imports: [
     CommonModule,
