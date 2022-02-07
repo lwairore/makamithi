c3506618a046c81ed502836a7c215605afaa0a0f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 16:04:26 +0300

    - Update how accordion is displayed.

--- src/app/about-us/sections/faq/faq.component.ts
+++ src/app/about-us/sections/faq/faq.component.ts
@@ -9,6 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class FaqComponent implements OnInit {
   collapsing = true;
 
+  faqs = [
+    {
+      title: 'Why Do You Eat Orange Food?',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat aute irure aliquam quaerat.'
+    },
+    {
+      title: 'Why Orange Food Is The Best For Health?',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat aute irure aliquam quaerat.'
+    },
+    {
+      title: 'Good Food For Good Health?',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat aute irure aliquam quaerat.'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
