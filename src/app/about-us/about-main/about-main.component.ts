0ec38c1722ca5527a74eb4409873092e640fc77a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 11:00:31 +0300

    - Generate component 'AboutMainComponent'.

---
+++ src/app/about-us/about-main/about-main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-about-main',
+  templateUrl: './about-main.component.html',
+  styles: [
+  ]
+})
+export class AboutMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutUsRoutingModule } from './about-us-routing.module';
+import { AboutMainComponent } from './about-main/about-main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AboutMainComponent],
   imports: [
     CommonModule,
     AboutUsRoutingModule
