53b147d376d525ba5892ef4efefc015f9ed19fd5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:19:07 +0300

    - Import module 'SharedModule'.

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopRoutingModule } from './shop-routing.module';
 import { ShopComponent } from './shop.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    
-  ShopComponent],
+    ShopComponent,
+  ],
   imports: [
     CommonModule,
-    ShopRoutingModule
+    ShopRoutingModule,
+    SharedModule,
   ]
 })
 export class ShopModule { }
