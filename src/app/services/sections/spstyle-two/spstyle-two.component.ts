7d216fd15c4d6946712a1a8fb0d4a73b086f0959
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 20:08:02 +0300

    - Generate component 'SPStyleTwoComponent'.

---
+++ src/app/services/sections/spstyle-two/spstyle-two.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-spstyle-two',
+  templateUrl: './spstyle-two.component.html',
+  styles: [
+  ]
+})
+export class SPStyleTwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -4,12 +4,14 @@ import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesComponent } from './services.component';
 import { SharedModule } from '../shared/shared.module';
 import { SPStyleOneComponent } from './sections/spstyle-one/spstyle-one.component';
+import { SPStyleTwoComponent } from './sections/spstyle-two/spstyle-two.component';
 
 
 @NgModule({
   declarations: [
     ServicesComponent,
     SPStyleOneComponent,
+    SPStyleTwoComponent,
   ],
   imports: [
     CommonModule,
