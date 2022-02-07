2950a835a48d33311a33a226025347888fc39fb4
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:21:42 +0300

    - Generate component 'ShopMainComponent'.

---
+++ src/app/shop/shop-main/shop-main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-shop-main',
+  templateUrl: './shop-main.component.html',
+  styles: [
+  ]
+})
+export class ShopMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ShopRoutingModule } from './shop-routing.module';
 import { ShopComponent } from './shop.component';
 import { SharedModule } from '../shared/shared.module';
+import { ShopMainComponent } from './shop-main/shop-main.component';
 
 
 @NgModule({
   declarations: [
     ShopComponent,
+    ShopMainComponent,
   ],
   imports: [
     CommonModule,
