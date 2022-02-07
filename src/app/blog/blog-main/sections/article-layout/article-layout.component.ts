180fe5cefa1edb83d422a234c9b38d27ea678757
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 22:05:55 +0300

    - Initialize object 'post'.

--- src/app/blog/blog-main/sections/article-layout/article-layout.component.ts
+++ src/app/blog/blog-main/sections/article-layout/article-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mak-pit-article-layout',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ArticleLayoutComponent implements OnInit {
+  @Input() post = {}
 
   constructor() { }
 
