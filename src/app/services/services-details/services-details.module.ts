912005742eb2ae9ac17314be2eb731c0b4f3e421
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 20:37:31 +0300

    - Generate module 'ServicesDetailsModule'.

---
+++ src/app/services/services-details/services-details.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ServicesDetailsRoutingModule } from './services-details-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ServicesDetailsRoutingModule
+  ]
+})
+export class ServicesDetailsModule { }
