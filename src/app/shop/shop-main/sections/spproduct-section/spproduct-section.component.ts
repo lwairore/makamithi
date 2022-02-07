844a9f4f4cf8c5495089e39b42e780017616d8e8
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:24:23 +0300

    - Generate component 'SPProductSectionComponent'.

---
+++ src/app/shop/shop-main/sections/spproduct-section/spproduct-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-spproduct-section',
+  templateUrl: './spproduct-section.component.html',
+  styles: [
+  ]
+})
+export class SPProductSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -4,12 +4,14 @@ import { ShopRoutingModule } from './shop-routing.module';
 import { ShopComponent } from './shop.component';
 import { SharedModule } from '../shared/shared.module';
 import { ShopMainComponent } from './shop-main/shop-main.component';
+import { SPProductSectionComponent } from './shop-main/sections/spproduct-section/spproduct-section.component';
 
 
 @NgModule({
   declarations: [
     ShopComponent,
     ShopMainComponent,
+    SPProductSectionComponent,
   ],
   imports: [
     CommonModule,
