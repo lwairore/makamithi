79b908f86ae371c64bfb6e6486035e3a8fb0385a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:44:01 +0300

    - Format module.

--- src/app/shop/shop-details/shop-details.module.ts
+++ src/app/shop/shop-details/shop-details.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ShopDetailsRoutingModule } from './shop-details-routing.module';
 import { ShopDetailsMainComponent } from './shop-details-main/shop-details-main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
