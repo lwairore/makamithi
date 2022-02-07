2e03ef4427cfc8d0e6d6341c290e8e1f6015fdf7
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 11:26:01 +0300

    - Initialize input variable 'pageTitle'.

--- src/app/about-us/sections/site-breadcrumb/site-breadcrumb.component.ts
+++ src/app/about-us/sections/site-breadcrumb/site-breadcrumb.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mak-pit-site-breadcrumb',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SiteBreadcrumbComponent implements OnInit {
+  @Input() pageTitle = '';
 
   constructor() { }
 
