040e20d3c3e5b5a5ac95563d82828d57baa4314b
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 18:05:48 +0300

    - Format module.

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesComponent } from './services.component';
 import { SharedModule } from '../shared/shared.module';
