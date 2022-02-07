90d95d3781e997ab2e146d871f8e55e2706e06a6
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 11:33:01 +0300

    - Move component 'SiteBreadcrumbComponent' to module 'SharedModule'.

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutMainComponent } from './about-main/about-main.component';
-import { SiteBreadcrumbComponent } from './sections/site-breadcrumb/site-breadcrumb.component';
 import { APAbout4SectionComponent } from './sections/apabout4-section/apabout4-section.component';
 
 
 @NgModule({
-  declarations: [AboutMainComponent, SiteBreadcrumbComponent, APAbout4SectionComponent],
+  declarations: [
+    AboutMainComponent,
+    APAbout4SectionComponent,
+  ],
   imports: [
     CommonModule,
     AboutUsRoutingModule
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SiteBreadcrumbComponent } from './site-breadcrumb/site-breadcrumb.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SiteBreadcrumbComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SiteBreadcrumbComponent,
   ]
 })
 export class SharedModule { }
