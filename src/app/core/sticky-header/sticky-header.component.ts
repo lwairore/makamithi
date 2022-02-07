85499302b6cc06790a986ed111c3be3c456fa992
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 17:20:22 +0300

    - Initialize boolean 'menuOpen' set to 'false'.

--- src/app/core/sticky-header/sticky-header.component.ts
+++ src/app/core/sticky-header/sticky-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mak-pit-sticky-header',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class StickyHeaderComponent implements OnInit {
+  @Input() menuOpen = false;
 
   constructor() { }
 
