2d03095e50ee2c6168afdc6900f8a037e63ad092
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 20:40:10 +0300

    - Generate component 'ServiceDetailsMainComponent'.

---
+++ src/app/services/services-details/service-details-main/service-details-main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-service-details-main',
+  templateUrl: './service-details-main.component.html',
+  styles: [
+  ]
+})
+export class ServiceDetailsMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/services/services-details/services-details.module.ts
+++ src/app/services/services-details/services-details.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ServicesDetailsRoutingModule } from './services-details-routing.module';
+import { ServiceDetailsMainComponent } from './service-details-main/service-details-main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ServiceDetailsMainComponent],
   imports: [
     CommonModule,
     ServicesDetailsRoutingModule
