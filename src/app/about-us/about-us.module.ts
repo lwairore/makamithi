72d054e535b9ca472d045f3cc63abbd019a8be8c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:37:22 +0300

    - Improve app structure.

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutMainComponent } from './about-main/about-main.component';
-import { APAbout4SectionComponent } from './sections/apabout4-section/apabout4-section.component';
+import { APAbout4SectionComponent } from './about-main/sections/apabout4-section/apabout4-section.component';
 import { SharedModule } from '../shared/shared.module';
-import { FaqComponent } from './sections/faq/faq.component';
+import { FaqComponent } from './about-main/sections/faq/faq.component';
 import { AccordionModule } from '../accordion/accordion.module';
-import { ApWhatWeDo2SectionComponent } from './sections/ap-what-we-do2-section/ap-what-we-do2-section.component';
-import { APTeam4SectionComponent } from './sections/apteam4-section/apteam4-section.component';
-import { ClientSlider2Component } from './sections/client-slider2/client-slider2.component';
+import { ApWhatWeDo2SectionComponent } from './about-main/sections/ap-what-we-do2-section/ap-what-we-do2-section.component';
+import { APTeam4SectionComponent } from './about-main/sections/apteam4-section/apteam4-section.component';
+import { ClientSlider2Component } from './about-main/sections/client-slider2/client-slider2.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 
 
