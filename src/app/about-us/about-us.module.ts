0f792b1893c6d6619db687d63646520b4e41bb13
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 13:51:45 +0300

    - Remove component 'AccordionComponent'.

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -5,7 +5,6 @@ import { AboutMainComponent } from './about-main/about-main.component';
 import { APAbout4SectionComponent } from './sections/apabout4-section/apabout4-section.component';
 import { SharedModule } from '../shared/shared.module';
 import { FaqComponent } from './sections/faq/faq.component';
-import { AccordionComponent } from './sections/accordion/accordion.component';
 
 
 @NgModule({
@@ -13,7 +12,6 @@ import { AccordionComponent } from './sections/accordion/accordion.component';
     AboutMainComponent,
     APAbout4SectionComponent,
     FaqComponent,
-    AccordionComponent,
   ],
   imports: [
     CommonModule,
