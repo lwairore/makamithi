ae72d190cc9d4c8836dc519302566607e58d40cc
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 13:52:11 +0300

    - Import module 'AccordionModule'.

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -5,6 +5,7 @@ import { AboutMainComponent } from './about-main/about-main.component';
 import { APAbout4SectionComponent } from './sections/apabout4-section/apabout4-section.component';
 import { SharedModule } from '../shared/shared.module';
 import { FaqComponent } from './sections/faq/faq.component';
+import { AccordionModule } from '../accordion/accordion.module';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { FaqComponent } from './sections/faq/faq.component';
     CommonModule,
     AboutUsRoutingModule,
     SharedModule,
+    AccordionModule,
   ]
 })
 export class AboutUsModule { }
