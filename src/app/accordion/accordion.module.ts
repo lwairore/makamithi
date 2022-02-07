4d523bcbc274fa79518460537b6ce33ab8fad583
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 13:03:22 +0300

    - Generate directive 'AccordionTitleDirective'.

--- src/app/accordion/accordion.module.ts
+++ src/app/accordion/accordion.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccordionItemDirective } from './directives/accordion-item.directive';
 import { AccordionContentDirective } from './directives/accordion-content.directive';
+import { AccordionTitleDirective } from './directives/accordion-title.directive';
 
 
 
 @NgModule({
-  declarations: [AccordionItemDirective, AccordionContentDirective],
+  declarations: [AccordionItemDirective, AccordionContentDirective, AccordionTitleDirective],
   imports: [
     CommonModule
   ]
