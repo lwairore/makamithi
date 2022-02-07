ac66ae32dfc14c5817dee0467eedc02791e96f60
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 11:38:28 +0300

    - Generate component 'FaqComponent'.

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -4,12 +4,14 @@ import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutMainComponent } from './about-main/about-main.component';
 import { APAbout4SectionComponent } from './sections/apabout4-section/apabout4-section.component';
 import { SharedModule } from '../shared/shared.module';
+import { FaqComponent } from './sections/faq/faq.component';
 
 
 @NgModule({
   declarations: [
     AboutMainComponent,
     APAbout4SectionComponent,
+    FaqComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/about-us/sections/faq/faq.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-faq',
+  templateUrl: './faq.component.html',
+  styles: [
+  ]
+})
+export class FaqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
