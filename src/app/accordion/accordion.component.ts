5450e1311e91ea3efc00468f0b755cbd0a47171e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 13:20:23 +0300

    - Initialize 'expanded'.

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AccordionComponent implements OnInit {
+  // A Set is used to maintain the state of currently expanded
+  // accordion items. A set guarantees distinct values.
+  expanded = new Set<number>();
+  /**
+   * Decides if the single item will be open at once or not.
+   * In collapsing mode, toggling one would collapse others
+   */
 
   constructor() { }
 
