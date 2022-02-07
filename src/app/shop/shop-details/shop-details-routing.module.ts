8b3da85d5c4a4a0c9c73eb9b7bdfd306c66f9f03
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:42:55 +0300

    - Map component 'ShopDetailsMainComponent' to a 'route'.

--- src/app/shop/shop-details/shop-details-routing.module.ts
+++ src/app/shop/shop-details/shop-details-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShopDetailsMainComponent } from './shop-details-main/shop-details-main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShopDetailsMainComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
