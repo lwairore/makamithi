9ae8a1e4f19b5a428595a63da4738979215464d3
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 17:47:03 +0300

    - Organize module 'CoreModule'.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './header/sections/top-header/top-header.component';
 import { BurgerMenusComponent } from './common/burger-menus/burger-menus.component';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
-import { SearchBarComponent } from './header/sections/search-bar/search-bar.component';
+import { SearchBarComponent } from './common/search-bar/search-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { HeaderStyleTwoComponent } from './header-style-two/header-style-two.component';
