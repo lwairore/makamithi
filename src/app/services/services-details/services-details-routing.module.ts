f27b0588081cf38f6a527dbe30ef62e178c57b3c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 20:41:00 +0300

    - Map component 'ServiceDetailsMainComponent' to a 'route'.

--- src/app/services/services-details/services-details-routing.module.ts
+++ src/app/services/services-details/services-details-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ServiceDetailsMainComponent } from './service-details-main/service-details-main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ServiceDetailsMainComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
