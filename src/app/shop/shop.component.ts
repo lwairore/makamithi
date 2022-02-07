0e24cc3384cbdf1a0a56977683370b457b5b57a4
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 21:17:36 +0300

    - Generate component 'ShopComponent'.

---
+++ src/app/shop/shop.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-shop',
+  templateUrl: './shop.component.html',
+  styles: [
+  ]
+})
+export class ShopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
