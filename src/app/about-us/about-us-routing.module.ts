6f6957e55611bb038e710713c9916e98d34a9db6
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-07 11:01:16 +0300

    - Map component 'AboutMainComponent' to a 'route'.

--- src/app/about-us/about-us-routing.module.ts
+++ src/app/about-us/about-us-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutMainComponent } from './about-main/about-main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutMainComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
