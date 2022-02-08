ca9bd5249105f4ee518bf2c191ab60fb5f8becb2
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 15:16:34 +0300

    - Import module 'TabsModule'.

--- src/app/shop/shop-details/shop-details.module.ts
+++ src/app/shop/shop-details/shop-details.module.ts
@@ -4,6 +4,7 @@ import { ShopDetailsRoutingModule } from './shop-details-routing.module';
 import { ShopDetailsMainComponent } from './shop-details-main/shop-details-main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SPShopDetailsSectionComponent } from './shop-details-main/sections/spshop-details-section/spshop-details-section.component';
+import { TabsModule } from 'src/app/lib/tabs/tabs.module';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { SPShopDetailsSectionComponent } from './shop-details-main/sections/spsh
     CommonModule,
     ShopDetailsRoutingModule,
     SharedModule,
+    TabsModule,
   ]
 })
 export class ShopDetailsModule { }
