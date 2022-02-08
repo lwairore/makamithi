59c54446966ea574d94b20f176f37b90dda72f8c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 14:23:20 +0300

    - Generate module 'GalleryDetails1Module'.

---
+++ src/app/gallery-two/gallery-details1/gallery-details1.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GalleryDetails1RoutingModule } from './gallery-details1-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GalleryDetails1RoutingModule
+  ]
+})
+export class GalleryDetails1Module { }
