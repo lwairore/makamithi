e8ad21b95ad5c8de3f65bfe363fc7c685e0c1e13
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 09:00:54 +0300

    - Remove component 'BlogDetailsComponent'.

--- src/app/blog/blog-details/blog-details.module.ts
+++ src/app/blog/blog-details/blog-details.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogDetailsRoutingModule } from './blog-details-routing.module';
-import { BlogDetailsComponent } from './blog-details.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BlogDetailsMainComponent } from './blog-details-main/blog-details-main.component';
 
 
 @NgModule({
   declarations: [
-    BlogDetailsComponent,
     BlogDetailsMainComponent,
   ],
   imports: [
