d54cd53dbd7a4a1a8d2462ce9ffc4f1af3702314
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 10:18:41 +0300

    - Generate component 'PageNotFoundComponent'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
