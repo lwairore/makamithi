9c6c6424bbbb76e23030131eebbb45231b2564aa
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 10:04:36 +0300

    - Format module.

--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TeamRoutingModule } from './team-routing.module';
 import { TeamMainComponent } from './team-main/team-main.component';
 
 
 @NgModule({
-  declarations: [TeamMainComponent],
+  declarations: [
+    TeamMainComponent,
+  ],
   imports: [
     CommonModule,
     TeamRoutingModule
