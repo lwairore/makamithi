8e0510eb6a78a3c5376a27eb8943d24622690add
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 15:00:24 +0300

    - Generate component 'GPGalleryDetailsOneSectionComponent'.

---
+++ src/app/gallery-two/gallery-details1/gallery-details-one-main/sections/gpgallery-details-one-section/gpgallery-details-one-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-gpgallery-details-one-section',
+  templateUrl: './gpgallery-details-one-section.component.html',
+  styles: [
+  ]
+})
+export class GPGalleryDetailsOneSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/gallery-two/gallery-details1/gallery-details1.module.ts
+++ src/app/gallery-two/gallery-details1/gallery-details1.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { GalleryDetails1RoutingModule } from './gallery-details1-routing.module';
 import { GalleryDetailsOneMainComponent } from './gallery-details-one-main/gallery-details-one-main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { GPGalleryDetailsOneSectionComponent } from './gallery-details-one-main/sections/gpgallery-details-one-section/gpgallery-details-one-section.component';
 
 
 @NgModule({
   declarations: [
     GalleryDetailsOneMainComponent,
+    GPGalleryDetailsOneSectionComponent,
   ],
   imports: [
     CommonModule,
