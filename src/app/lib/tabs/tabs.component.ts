5251a7dfa6c9a3260127f4f3f36f0564337e7a2a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 11:13:10 +0300

    - Generate module 'TabsModule'.

---
+++ src/app/lib/tabs/tabs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-tabs',
+  templateUrl: './tabs.component.html',
+  styles: [
+  ]
+})
+export class TabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/lib/tabs/tabs.module.ts
+++ src/app/lib/tabs/tabs.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TabListDirective } from './directives/tab-list.directive';
 import { TabDirective } from './directives/tab.directive';
 import { TabPanelDirective } from './directives/tab-panel.directive';
+import { TabsComponent } from './tabs.component';
 
 
 
 @NgModule({
-  declarations: [TabListDirective, TabDirective, TabPanelDirective],
+  declarations: [TabListDirective, TabDirective, TabPanelDirective, TabsComponent],
   imports: [
     CommonModule
   ]
