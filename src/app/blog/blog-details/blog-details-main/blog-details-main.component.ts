50031306fb89b08fcf37e8c2b11b171b1d6e7b1a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 09:01:06 +0300

    - Initialize 'blogDetails'.

--- src/app/blog/blog-details/blog-details-main/blog-details-main.component.ts
+++ src/app/blog/blog-details/blog-details-main/blog-details-main.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BlogDetailsMainComponent implements OnInit {
+  blogDetails = {
+    "id": "3",
+    "title": "Monthly Web Development To Update React Hooks Cons Wether Says",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.",
+    "image": "assets/img/blog/b3.jpg",
+    "url": "/monthly-web-development-to-update-react-hooks-cons-wether-says-3",
+    "date": "January 1, 2022"
+  }
 
   constructor() { }
 
