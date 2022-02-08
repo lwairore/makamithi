43e1e009536084893ab9b4acfb60624e21e1342d
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 10:08:04 +0300

    - Generate component 'TeamSectionComponent'.

---
+++ src/app/team/team-main/sections/team-section/team-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-team-section',
+  templateUrl: './team-section.component.html',
+  styles: [
+  ]
+})
+export class TeamSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { TeamRoutingModule } from './team-routing.module';
 import { TeamMainComponent } from './team-main/team-main.component';
 import { SharedModule } from '../shared/shared.module';
+import { TeamSectionComponent } from './team-main/sections/team-section/team-section.component';
 
 
 @NgModule({
   declarations: [
     TeamMainComponent,
+    TeamSectionComponent,
   ],
   imports: [
     CommonModule,
