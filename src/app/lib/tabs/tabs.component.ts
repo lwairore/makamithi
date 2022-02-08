30abf63a674a6731e0f883293c071ac7b8078f21
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 13:31:32 +0300

    - Create method 'isPanelSelected'.

--- src/app/lib/tabs/tabs.component.ts
+++ src/app/lib/tabs/tabs.component.ts
@@ -1,5 +1,8 @@
-import { Component, ContentChildren, Input, OnInit, QueryList } from '@angular/core';
+import { Component, ContentChild, ContentChildren, Input, OnInit, QueryList } from '@angular/core';
+import { memoize } from 'lodash';
 import { TabPanelDirective } from './directives/tab-panel.directive';
+import { TabDirective } from './directives/tab.directive';
+import { TabListComponent } from './tab-list/tab-list.component';
 
 @Component({
   selector: 'mak-pit-tabs',
@@ -15,9 +18,22 @@ export class TabsComponent implements OnInit {
   @ContentChildren(TabPanelDirective)
   panels: QueryList<TabPanelDirective>;
 
+  @ContentChild(TabListComponent)
+  tabList: TabListComponent;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  isPanelSelected = memoize((panelLoopIndex: number) => {
+    const panelTab = this.tabList.tabItems.toArray()[panelLoopIndex];
+
+    if (!(panelTab instanceof TabDirective)) {
+      return false;
+    }
+
+    return panelTab.active;
+  })
 
 }
