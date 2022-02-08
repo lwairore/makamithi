15de2656d13efae6954abd5b84c492176b46c9ae
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 11:37:51 +0300

    - Remove directive 'TabListDirective'.

--- src/app/lib/tabs/tabs.module.ts
+++ src/app/lib/tabs/tabs.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TabListDirective } from './directives/tab-list.directive';
 import { TabDirective } from './directives/tab.directive';
 import { TabPanelDirective } from './directives/tab-panel.directive';
 import { TabsComponent } from './tabs.component';
@@ -11,7 +10,6 @@ import { TabListComponent } from './tab-list/tab-list.component';
 
 @NgModule({
   declarations: [
-    TabListDirective,
     TabDirective,
     TabPanelDirective,
     TabsComponent,
@@ -23,7 +21,6 @@ import { TabListComponent } from './tab-list/tab-list.component';
   ],
   exports: [
     TabsComponent,
-    TabListDirective,
     TabDirective,
     TabPanelDirective,
     TabItemDirective,
