7c00296880a931d5d92995036430d81d9da49017
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 20:36:12 +0300

    - Generate component 'GalleryComponent'.

---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-gallery',
+  templateUrl: './gallery.component.html',
+  styles: [
+  ]
+})
+export class GalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
