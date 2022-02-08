a7c7bf235d776bb889ba488596e7f762f767c380
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 08:46:37 +0300

    - Generate component 'BlogDetailsComponent'.

---
+++ src/app/blog/blog-details/blog-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-blog-details',
+  templateUrl: './blog-details.component.html',
+  styles: [
+  ]
+})
+export class BlogDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/blog/blog-details/blog-details.module.ts
+++ src/app/blog/blog-details/blog-details.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogDetailsRoutingModule } from './blog-details-routing.module';
+import { BlogDetailsComponent } from './blog-details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BlogDetailsComponent],
   imports: [
     CommonModule,
     BlogDetailsRoutingModule
