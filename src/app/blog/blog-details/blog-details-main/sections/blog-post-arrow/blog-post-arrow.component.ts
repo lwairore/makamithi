b7475f02ff6a5b08f23da3b5940686a7e6653097
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 09:04:55 +0300

    - Generate component 'BlogPostArrowComponent'.

---
+++ src/app/blog/blog-details/blog-details-main/sections/blog-post-arrow/blog-post-arrow.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-blog-post-arrow',
+  templateUrl: './blog-post-arrow.component.html',
+  styles: [
+  ]
+})
+export class BlogPostArrowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/blog/blog-details/blog-details.module.ts
+++ src/app/blog/blog-details/blog-details.module.ts
@@ -4,12 +4,14 @@ import { BlogDetailsRoutingModule } from './blog-details-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BlogDetailsMainComponent } from './blog-details-main/blog-details-main.component';
 import { BlogPostRelatedComponent } from './blog-details-main/sections/blog-post-related/blog-post-related.component';
+import { BlogPostArrowComponent } from './blog-details-main/sections/blog-post-arrow/blog-post-arrow.component';
 
 
 @NgModule({
   declarations: [
     BlogDetailsMainComponent,
     BlogPostRelatedComponent,
+    BlogPostArrowComponent,
   ],
   imports: [
     CommonModule,
