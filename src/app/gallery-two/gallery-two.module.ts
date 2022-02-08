1a4b404f9492b83071ae4927fd36d3caacf5e6ef
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 14:00:12 +0300

    - Generate component 'GalleryTwoComponent'.

--- src/app/gallery-two/gallery-two.module.ts
+++ src/app/gallery-two/gallery-two.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryTwoRoutingModule } from './gallery-two-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { GalleryTwoComponent } from './gallery-two.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GalleryTwoComponent],
   imports: [
     CommonModule,
     GalleryTwoRoutingModule,
