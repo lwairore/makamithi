67013295a2fd21eff91a208bd319efe793c5fd81
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 10:14:27 +0300

    - Generate component 'GPCtaSectionComponent'.

---
+++ src/app/team/team-main/sections/gpcta-section/gpcta-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-gpcta-section',
+  templateUrl: './gpcta-section.component.html',
+  styles: [
+  ]
+})
+export class GPCtaSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -4,12 +4,14 @@ import { TeamRoutingModule } from './team-routing.module';
 import { TeamMainComponent } from './team-main/team-main.component';
 import { SharedModule } from '../shared/shared.module';
 import { TeamSectionComponent } from './team-main/sections/team-section/team-section.component';
+import { GPCtaSectionComponent } from './team-main/sections/gpcta-section/gpcta-section.component';
 
 
 @NgModule({
   declarations: [
     TeamMainComponent,
     TeamSectionComponent,
+    GPCtaSectionComponent,
   ],
   imports: [
     CommonModule,
