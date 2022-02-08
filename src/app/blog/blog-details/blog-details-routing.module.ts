14a5d5507ff72cb71f61018e0a39c334716957a5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 08:47:22 +0300

    - Map component 'BlogDetailsComponent' to a 'route'.

--- src/app/blog/blog-details/blog-details-routing.module.ts
+++ src/app/blog/blog-details/blog-details-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogDetailsComponent } from './blog-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BlogDetailsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
