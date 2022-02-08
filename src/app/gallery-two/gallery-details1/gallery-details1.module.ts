5eab8b548013929fe743c518897321cb01426b58
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 14:56:05 +0300

    - Format module.

--- src/app/gallery-two/gallery-details1/gallery-details1.module.ts
+++ src/app/gallery-two/gallery-details1/gallery-details1.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryDetails1RoutingModule } from './gallery-details1-routing.module';
+import { GalleryDetailsOneMainComponent } from './gallery-details-one-main/gallery-details-one-main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GalleryDetailsOneMainComponent,
+  ],
   imports: [
     CommonModule,
     GalleryDetails1RoutingModule
