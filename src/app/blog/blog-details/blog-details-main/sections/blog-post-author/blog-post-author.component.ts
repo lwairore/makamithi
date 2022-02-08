4fa3663adea2612c1c979f4a4d947e6a9bce69a3
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 09:06:38 +0300

    - Generate component 'BlogPostAuthorComponent'.

---
+++ src/app/blog/blog-details/blog-details-main/sections/blog-post-author/blog-post-author.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-blog-post-author',
+  templateUrl: './blog-post-author.component.html',
+  styles: [
+  ]
+})
+export class BlogPostAuthorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/blog/blog-details/blog-details.module.ts
+++ src/app/blog/blog-details/blog-details.module.ts
@@ -5,6 +5,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { BlogDetailsMainComponent } from './blog-details-main/blog-details-main.component';
 import { BlogPostRelatedComponent } from './blog-details-main/sections/blog-post-related/blog-post-related.component';
 import { BlogPostArrowComponent } from './blog-details-main/sections/blog-post-arrow/blog-post-arrow.component';
+import { BlogPostAuthorComponent } from './blog-details-main/sections/blog-post-author/blog-post-author.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { BlogPostArrowComponent } from './blog-details-main/sections/blog-post-a
     BlogDetailsMainComponent,
     BlogPostRelatedComponent,
     BlogPostArrowComponent,
+    BlogPostAuthorComponent,
   ],
   imports: [
     CommonModule,
