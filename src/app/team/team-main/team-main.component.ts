66400fdd98b3864168d42d22442640438b42ec6f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 10:03:34 +0300

    - Generate component 'TeamMainComponent'.

---
+++ src/app/team/team-main/team-main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-team-main',
+  templateUrl: './team-main.component.html',
+  styles: [
+  ]
+})
+export class TeamMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TeamRoutingModule } from './team-routing.module';
+import { TeamMainComponent } from './team-main/team-main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TeamMainComponent],
   imports: [
     CommonModule,
     TeamRoutingModule
