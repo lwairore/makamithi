a43aaa6c872fec3e5d1e67e3e978ef3ab1868f7c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 11:01:48 +0300

    - Import module 'SharedModule'.

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryRoutingModule } from './gallery-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    GalleryRoutingModule
+    GalleryRoutingModule,
+    SharedModule,
   ]
 })
 export class GalleryModule { }
