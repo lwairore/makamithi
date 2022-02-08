e482c8a3793c1d085682dda2a31c3cc2369f0737
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 14:59:30 +0300

    - Generate component 'GalleryDetailsOneMainComponent'.

---
+++ src/app/gallery-two/gallery-details1/gallery-details-one-main/gallery-details-one-main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-gallery-details-one-main',
+  templateUrl: './gallery-details-one-main.component.html',
+  styles: [
+  ]
+})
+export class GalleryDetailsOneMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
