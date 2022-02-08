5a71c6b76c4566966b6e2848af4c9b3ac30eb23e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 20:37:43 +0300

    - Format module.

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { GalleryRoutingModule } from './gallery-routing.module';
 import { GalleryComponent } from './gallery.component';
 import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [GalleryComponent],
+  declarations: [
+    GalleryComponent,
+  ],
   imports: [
     CommonModule,
     GalleryRoutingModule,
