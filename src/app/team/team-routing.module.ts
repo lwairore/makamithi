75649245ce22c65ffaf007680e1719e4f03f6c69
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 10:04:14 +0300

    - Map component 'TeamMainComponent' to a 'route'.

--- src/app/team/team-routing.module.ts
+++ src/app/team/team-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TeamMainComponent } from './team-main/team-main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TeamMainComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
