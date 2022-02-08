c0f736677aa635428922d5698d3c458536951e37
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 11:03:50 +0300

    - Map component 'GalleryComponent' to a 'route'.

--- src/app/gallery/gallery-routing.module.ts
+++ src/app/gallery/gallery-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GalleryComponent } from './gallery.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: GalleryComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
