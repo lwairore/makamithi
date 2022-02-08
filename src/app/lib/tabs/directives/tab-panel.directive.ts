74c3bb460d7bfc868be0ba83f326a478a9d6ab07
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 11:12:13 +0300

    - Generate directive 'TabPanelDirective'.

---
+++ src/app/lib/tabs/directives/tab-panel.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[makPitTabPanel]'
+})
+export class TabPanelDirective {
+
+  constructor() { }
+
+}
--- src/app/lib/tabs/tabs.module.ts
+++ src/app/lib/tabs/tabs.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabListDirective } from './directives/tab-list.directive';
 import { TabDirective } from './directives/tab.directive';
+import { TabPanelDirective } from './directives/tab-panel.directive';
 
 
 
 @NgModule({
-  declarations: [TabListDirective, TabDirective],
+  declarations: [TabListDirective, TabDirective, TabPanelDirective],
   imports: [
     CommonModule
   ]
