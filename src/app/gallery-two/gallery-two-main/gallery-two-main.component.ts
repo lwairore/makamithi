e440ab29ea746eaeaff688f3308318c99a7a5533
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 14:05:16 +0300

    - Generate component 'GalleryTwoMainComponent'.

---
+++ src/app/gallery-two/gallery-two-main/gallery-two-main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-gallery-two-main',
+  templateUrl: './gallery-two-main.component.html',
+  styles: [
+  ]
+})
+export class GalleryTwoMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
