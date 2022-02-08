2643bc6b86d322ccf7a3edfab2b6205197b07dff
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 14:06:17 +0300

    - Generate component 'GpGalleryTwoSectionComponent'.

---
+++ src/app/gallery-two/gallery-two-main/sections/gp-gallery-two-section/gp-gallery-two-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-gp-gallery-two-section',
+  templateUrl: './gp-gallery-two-section.component.html',
+  styles: [
+  ]
+})
+export class GpGalleryTwoSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/gallery-two/gallery-two.module.ts
+++ src/app/gallery-two/gallery-two.module.ts
@@ -4,10 +4,11 @@ import { GalleryTwoRoutingModule } from './gallery-two-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { GalleryTwoComponent } from './gallery-two.component';
 import { GalleryTwoMainComponent } from './gallery-two-main/gallery-two-main.component';
+import { GpGalleryTwoSectionComponent } from './gallery-two-main/sections/gp-gallery-two-section/gp-gallery-two-section.component';
 
 
 @NgModule({
-  declarations: [GalleryTwoComponent, GalleryTwoMainComponent],
+  declarations: [GalleryTwoComponent, GalleryTwoMainComponent, GpGalleryTwoSectionComponent],
   imports: [
     CommonModule,
     GalleryTwoRoutingModule,
