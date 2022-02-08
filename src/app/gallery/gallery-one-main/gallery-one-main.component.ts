e3ffb4690e02923035c28df0f890dcf41ae642de
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 20:39:06 +0300

    - Generate component 'GalleryOneMainComponent'.

---
+++ src/app/gallery/gallery-one-main/gallery-one-main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-gallery-one-main',
+  templateUrl: './gallery-one-main.component.html',
+  styles: [
+  ]
+})
+export class GalleryOneMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { GalleryRoutingModule } from './gallery-routing.module';
 import { GalleryComponent } from './gallery.component';
 import { SharedModule } from '../shared/shared.module';
+import { GalleryOneMainComponent } from './gallery-one-main/gallery-one-main.component';
 
 
 @NgModule({
   declarations: [
     GalleryComponent,
+    GalleryOneMainComponent,
   ],
   imports: [
     CommonModule,
