bc9d8630a6afae7f3ddf0382d5794ce1dbeabc4a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 11:03:16 +0300

    - Generate component 'GalleryComponent'.

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryRoutingModule } from './gallery-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { GalleryComponent } from './gallery.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GalleryComponent],
   imports: [
     CommonModule,
     GalleryRoutingModule,
