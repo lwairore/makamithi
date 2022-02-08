c629ddcbab044f91616bdae8ff61b1ad49721379
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-08 08:43:38 +0300

    - Generate module 'BlogDetailsModule'.

---
+++ src/app/blog/blog-details/blog-details.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BlogDetailsRoutingModule } from './blog-details-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BlogDetailsRoutingModule
+  ]
+})
+export class BlogDetailsModule { }
