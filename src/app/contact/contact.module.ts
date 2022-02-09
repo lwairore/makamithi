a7e7ddd01398e84e654fe234add5846417673e87
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-09 20:06:23 +0300

    - Format module 'ContactModule'.

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactsMainComponent } from './contacts-main/contacts-main.component';
 
 
 @NgModule({
-  declarations: [ContactsMainComponent],
+  declarations: [
+    ContactsMainComponent,
+  ],
   imports: [
     CommonModule,
     ContactRoutingModule
