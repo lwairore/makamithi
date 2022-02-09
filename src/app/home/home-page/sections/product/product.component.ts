bb3085c62b367d9cac91560c711c4e2e5daf2203
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-09 21:53:10 +0300

    - Update object 'products'.

--- src/app/home/home-page/sections/product/product.component.ts
+++ src/app/home/home-page/sections/product/product.component.ts
@@ -10,23 +10,133 @@ export class ProductComponent implements OnInit {
   products = [
     {
       flaticon: 'diet',
-      title: 'Animal feeds'
+      title: 'Animal feeds',
+      items: [
+        {
+          image: 'assets/img/product/product1.png',
+          title: 'Broccoli Head',
+          price: 5499
+        },
+        {
+          image: 'assets/img/product/product2.png',
+          title: 'Avocado Head',
+          price: 3299
+        },
+        {
+          image: 'assets/img/product/product3.png',
+          title: 'Breadfruit Head',
+          price: 2859
+        },
+        {
+          image: 'assets/img/product/product4.png',
+          title: 'Blackberries Head',
+          price: 4999
+        },
+      ] 
     },
     {
       flaticon: 'tomato',
-      title: 'Fertilizers'
+      title: 'Fertilizers',
+      items: [
+        {
+          image: 'assets/img/product/product4.png',
+          title: 'Blackberries Head',
+          price: 4999
+        },
+        {
+          image: 'assets/img/product/product1.png',
+          title: 'Broccoli Head',
+          price: 5499
+        },
+        {
+          image: 'assets/img/product/product2.png',
+          title: 'Avocado Head',
+          price: 3299
+        },
+        {
+          image: 'assets/img/product/product3.png',
+          title: 'Breadfruit Head',
+          price: 2859
+        },
+      ] 
     },
     {
       flaticon: 'pumpkin',
-      title: 'Seeds'
+      title: 'Seeds',
+      items: [
+        {
+          image: 'assets/img/product/product3.png',
+          title: 'Breadfruit Head',
+          price: 2859
+        },
+        {
+          image: 'assets/img/product/product4.png',
+          title: 'Blackberries Head',
+          price: 4999
+        },
+        {
+          image: 'assets/img/product/product1.png',
+          title: 'Broccoli Head',
+          price: 5499
+        },
+        {
+          image: 'assets/img/product/product2.png',
+          title: 'Avocado Head',
+          price: 3299
+        },
+      ] 
     },
     {
       flaticon: 'vegetarian',
-      title: 'Veterinary Products'
+      title: 'Veterinary Products',
+      items: [
+        {
+          image: 'assets/img/product/product1.png',
+          title: 'Broccoli Head',
+          price: 5499
+        },
+        {
+          image: 'assets/img/product/product3.png',
+          title: 'Breadfruit Head',
+          price: 2859
+        },
+        {
+          image: 'assets/img/product/product4.png',
+          title: 'Blackberries Head',
+          price: 4999
+        },
+        {
+          image: 'assets/img/product/product2.png',
+          title: 'Avocado Head',
+          price: 3299
+        },
+      ] 
     },
     {
       flaticon: 'diet',
-      title: 'Agro - Chemicals'
+      title: 'Agro - Chemicals',
+      items: [
+        {
+          image: 'assets/img/product/product4.png',
+          title: 'Blackberries Head',
+          price: 4999
+        },
+        {
+          image: 'assets/img/product/product1.png',
+          title: 'Broccoli Head',
+          price: 5499
+        },
+        {
+          image: 'assets/img/product/product2.png',
+          title: 'Avocado Head',
+          price: 3299
+        },
+        {
+          image: 'assets/img/product/product3.png',
+          title: 'Breadfruit Head',
+          price: 2859
+        },
+      ] 
     },
   ]
 
