e2d902d4a7dda131be2460d33e2a0ae3a92504cf
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-09 21:59:34 +0300

    - Update object 'listCoreValue'.

--- src/app/home/home-page/sections/h1-choose-us-section/h1-choose-us-section.component.ts
+++ src/app/home/home-page/sections/h1-choose-us-section/h1-choose-us-section.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class H1ChooseUsSectionComponent implements OnInit {
+  listCoreValue = [
+    {
+      title: 'Integrity',
+      image: 'assets/img/icon/1.png',
+      description: 'Sorem ipsum dolor sit amet consecta dipisicing elit sed do eiusmod tempor incidide.'
+    },
+    {
+      title: 'Team work',
+      image: 'assets/img/icon/2.png',
+      description: 'Sorem ipsum dolor sit amet consecta dipisicing elit sed do eiusmod tempor incidide.'
+    },
+    {
+      title: 'Efficiency',
+      image: 'assets/img/icon/3.png',
+      description: 'Sorem ipsum dolor sit amet consecta dipisicing elit sed do eiusmod tempor incidide.'
+    },
+    {
+      title: 'Professionalism',
+      image: 'assets/img/icon/1.png',
+      description: 'Sorem ipsum dolor sit amet consecta dipisicing elit sed do eiusmod tempor incidide.'
+    },
+    {
+      title: 'Respect for all',
+      image: 'assets/img/icon/2.png',
+      description: 'Sorem ipsum dolor sit amet consecta dipisicing elit sed do eiusmod tempor incidide.'
+    },
+    {
+      title: 'Social Responsible',
+      image: 'assets/img/icon/3.png',
+      description: 'Sorem ipsum dolor sit amet consecta dipisicing elit sed do eiusmod tempor incidide.'
+    },
+  ]
 
   constructor() { }
 
