40823c60497320889803847758e760b121648856
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-09 22:17:37 +0300

    - Initialize 'listService'.

--- src/app/about-us/about-main/sections/ap-what-we-do2-section/ap-what-we-do2-section.component.ts
+++ src/app/about-us/about-main/sections/ap-what-we-do2-section/ap-what-we-do2-section.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ApWhatWeDo2SectionComponent implements OnInit {
+  listService = [
+    {
+      image: 'assets/img/icon/icon1.png',
+      title: 'Technical support from our agronomists',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      image: 'assets/img/icon/icon2.png',
+      title: 'Consultation',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      image: 'assets/img/icon/icon3.png',
+      title: 'Farmers training',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      image: 'assets/img/icon/icon3.png',
+      title: 'Policy implementation',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      title: 'Business advisory',
+      image: 'assets/img/icon/icon1.png',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
+    },
+  ]
 
   constructor() { }
 
