25395ba76ddd5f7b36c280dc3d4498be1cefe015
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-09 20:51:16 +0300

    - Initialize object 'listPlans'.

--- src/app/services/service-one-main/sections/sppricing/sppricing.component.ts
+++ src/app/services/service-one-main/sections/sppricing/sppricing.component.ts
@@ -7,6 +7,85 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SPPricingComponent implements OnInit {
+  listPlans = [
+    {
+      image: 'assets/img/icon/p1.png',
+      title: 'Regular Plan',
+      price: 2855,
+      benefits: [
+        {
+          title: 'Money Back Guarantee',
+        },
+        {
+          title: 'India Organic Foods',
+        },
+        {
+          title: 'BD Pineaple Juice',
+        },
+        {
+          title: 'USA Organic Juice'
+        },
+        {
+          title: 'Fresh Food Item',
+        },
+        {
+          title: '100% Pure Item'
+        },
+      ]
+    },
+
+    {
+      image: 'assets/img/icon/p2.png',
+      price: 5055,
+      title: 'Premium Plan',
+      benefits: [
+        {
+          title: 'Money Back Guarantee',
+        },
+        {
+          title: 'India Organic Foods',
+        },
+        {
+          title: 'BD Pineaple Juice',
+        },
+        {
+          title: 'USA Organic Juice'
+        },
+        {
+          title: 'Fresh Food Item',
+        },
+        {
+          title: '100% Pure Item'
+        },
+      ]
+    },
+
+    {
+      image: 'assets/img/icon/p3.png',
+      price: 10555,
+      title: 'Diamond Plan',
+      benefits: [
+        {
+          title: 'Money Back Guarantee',
+        },
+        {
+          title: 'India Organic Foods',
+        },
+        {
+          title: 'BD Pineaple Juice',
+        },
+        {
+          title: 'USA Organic Juice'
+        },
+        {
+          title: 'Fresh Food Item',
+        },
+        {
+          title: '100% Pure Item'
+        },
+      ]
+    }
+  ]
 
   constructor() { }
 
