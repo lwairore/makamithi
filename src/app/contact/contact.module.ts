5cfc32c003ec672119c3489b1f56f9606e9b5e6e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-09 20:13:38 +0300

    - Generate component 'CPContactFormSection'.

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -4,12 +4,14 @@ import { ContactRoutingModule } from './contact-routing.module';
 import { ContactsMainComponent } from './contacts-main/contacts-main.component';
 import { SharedModule } from '../shared/shared.module';
 import { CPContactInfoSectionComponent } from './contacts-main/sections/cpcontact-info-section/cpcontact-info-section.component';
+import { CPContactFormSectionComponent } from './contacts-main/sections/cpcontact-form-section/cpcontact-form-section.component';
 
 
 @NgModule({
   declarations: [
     ContactsMainComponent,
     CPContactInfoSectionComponent,
+    CPContactFormSectionComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/contact/contacts-main/sections/cpcontact-form-section/cpcontact-form-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-cpcontact-form-section',
+  templateUrl: './cpcontact-form-section.component.html',
+  styles: [
+  ]
+})
+export class CPContactFormSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
