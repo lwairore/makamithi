cc4c790be5ecd2b1df509fad2fb85210d1a916ee
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-09 20:05:44 +0300

    - Map component 'ContactsMainComponent' to a 'route'.

--- src/app/contact/contact-routing.module.ts
+++ src/app/contact/contact-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactsMainComponent } from './contacts-main/contacts-main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContactsMainComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
