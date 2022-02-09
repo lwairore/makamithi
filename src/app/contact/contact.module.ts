9ce0b7847a2a644cf826b2b2c9f94e022f805afc
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-09 20:11:01 +0300

    - Generate component 'CPContactInfoSectionComponent'.

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactsMainComponent } from './contacts-main/contacts-main.component';
 import { SharedModule } from '../shared/shared.module';
+import { CPContactInfoSectionComponent } from './contacts-main/sections/cpcontact-info-section/cpcontact-info-section.component';
 
 
 @NgModule({
   declarations: [
     ContactsMainComponent,
+    CPContactInfoSectionComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/contact/contacts-main/sections/cpcontact-info-section/cpcontact-info-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mak-pit-cpcontact-info-section',
+  templateUrl: './cpcontact-info-section.component.html',
+  styles: [
+  ]
+})
+export class CPContactInfoSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
