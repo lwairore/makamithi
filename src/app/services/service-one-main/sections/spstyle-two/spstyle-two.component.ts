a30ca8e976e1d65dae6f9e6a2a47765aa3274224
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-09 20:37:06 +0300

    - Initialize variable 'listService'.

--- src/app/services/service-one-main/sections/spstyle-two/spstyle-two.component.ts
+++ src/app/services/service-one-main/sections/spstyle-two/spstyle-two.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SPStyleTwoComponent implements OnInit {
+  listService = [
+    {
+      image: 'assets/img/service/4.jpg',
+      title: 'Technical support from our agronomists',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      image: 'assets/img/service/5.jpg',
+      title: 'Consultation',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      image: 'assets/img/service/3.jpg',
+      title: 'Farmers training',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      image: 'assets/img/service/6.jpg',
+      title: 'Policy implementation',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      title: 'Business advisory',
+      image: 'assets/img/service/7.jpg',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
+    },
+  ]
+
 
   constructor() { }
 
