015d14be8139e8f51851ab4e6e44a1e0f32ea566
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 16:02:47 +0300

    - Add property 'photos'.

--- src/app/home/custom-types/banner-ad-format-http-response.ts
+++ src/app/home/custom-types/banner-ad-format-http-response.ts
@@ -1,4 +1,7 @@
+import { ItemPreviewFormatHttpResponse } from "./item-preview-format-http-response";
+
 export type BannerAdFormatHttpResponse = Readonly<{
     title: string;
     description: string;
+    photos: ReadonlyArray<ItemPreviewFormatHttpResponse>
 }>
