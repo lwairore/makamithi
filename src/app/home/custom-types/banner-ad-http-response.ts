73ec99ec66faef283c3df2762132f033046cd8d9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 15:58:00 +0300

    - Add optional property 'photos'.

--- src/app/home/custom-types/banner-ad-http-response.ts
+++ src/app/home/custom-types/banner-ad-http-response.ts
@@ -1,4 +1,7 @@
+import { ItemPreviewHttpResponse } from ".";
+
 export type BannerAdHttpResponse = Readonly<{
     title: string;
     description: string;
+    photos?: ReadonlyArray<ItemPreviewHttpResponse>;
 }>
