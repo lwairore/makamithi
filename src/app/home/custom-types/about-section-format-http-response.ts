bf048523c0eb9ba34f248a59fec7a222a28fcdad
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 20:10:39 +0300

    - Add property 'photo' of type 'AboutSectionFormatHttpResponse'.

--- src/app/home/custom-types/about-section-format-http-response.ts
+++ src/app/home/custom-types/about-section-format-http-response.ts
@@ -1,5 +1,8 @@
+import { ItemPreviewFormatHttpResponse } from "./item-preview-format-http-response";
+
 export type AboutSectionFormatHttpResponse = {
     heading: string;
     subheading: string;
     description: string;
+    photo: ItemPreviewFormatHttpResponse;
 }
