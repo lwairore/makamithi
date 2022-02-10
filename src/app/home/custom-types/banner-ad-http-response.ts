3a68cec63738a8b139d6dcdf3edfa6cbb814ddc9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 16:56:48 +0300

    - Rename property 'photos' to 'photo'. - Update the data type for property 'photo'.

--- src/app/home/custom-types/banner-ad-http-response.ts
+++ src/app/home/custom-types/banner-ad-http-response.ts
@@ -3,5 +3,5 @@ import { ItemPreviewHttpResponse } from "./item-preview-http-response";
 export type BannerAdHttpResponse = Readonly<{
     title?: string;
     description?: string;
-    photos?: ReadonlyArray<ItemPreviewHttpResponse>;
+    photo?: ItemPreviewHttpResponse;
 }>
