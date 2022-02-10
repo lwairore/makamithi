e5c8270638b2e1128318cafa0e405162db8eeaf1
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 16:38:47 +0300

    - Update method 'listBannerAds$'.

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,7 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { retryWithBackoff } from '@sharedModule/operators';
+import { convertItemToString, isObjectEmpty, stringIsEmpty } from '@sharedModule/utilities';
+import { memoize } from 'lodash';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { ItemPreviewFormatHttpResponse, ItemPreviewHttpResponse } from './custom-types';
+import { BannerAdFormatHttpResponse } from './custom-types/banner-ad-format-http-response';
+import { BannerAdHttpResponse } from './custom-types/banner-ad-http-response';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +18,57 @@ export class HomeService {
     private _httpClient: HttpClient
   ) { }
 
+  constructMediaSrc = memoize((src: any) => {
+    if (stringIsEmpty(src)) { return ''; }
+
+    const convertSrcToString = convertItemToString(src);
+
+    const newSrc = (environment.production)
+      ? convertSrcToString : `${environment.imageBaseURL}${convertSrcToString}`;
+
+    return newSrc;
+  })
+
+  private _formatShowcaseItemWithPhoto(
+    photoGallery?: ReadonlyArray<ItemPreviewHttpResponse>) {
+    if (!Array.isArray(photoGallery)) {
+      return Array();
+    }
+
+    const formatGallery = photoGallery
+      .map(galleryItem => {
+        const formatGalleryItem: ItemPreviewFormatHttpResponse = {
+          alt: convertItemToString(galleryItem.caption),
+          src: this.constructMediaSrc(galleryItem.image),
+        };
+
+        return formatGalleryItem;
+      }).filter(item => !isObjectEmpty(item));
+
+    return formatGallery;
+  }
+
   listBannerAds$() {
     const api = environment.baseURL +
       environment.home.rootURL +
       environment.home.bannerAd.listBannerAd();
 
-    return this._httpClient.get(
+    return this._httpClient.get<Array<BannerAdHttpResponse>>(
       api).pipe(
-        retryWithBackoff(1000, 5));
+        retryWithBackoff<Array<BannerAdHttpResponse>>(1000, 5),
+        map(ads => {
+          const formattedAds = ads.map(ad => {
+            const formattedAd: BannerAdFormatHttpResponse = {
+              title: convertItemToString(ad.title),
+              description: convertItemToString(ad.description),
+              photos: this._formatShowcaseItemWithPhoto(ad.photos)
+            };
+
+            return formattedAd;
+          }).filter(item => !isObjectEmpty(item));;
+
+          return formattedAds;
+        })
+      );
   }
 }
