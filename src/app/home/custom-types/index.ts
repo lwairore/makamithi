8d11d078812e63b315a5c32c49f0e0aec2750e42
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 20:11:41 +0300

    - Export type 'AboutSectionFormatHttpResponse'.

--- src/app/home/custom-types/index.ts
+++ src/app/home/custom-types/index.ts
@@ -1,3 +1,4 @@
 export { ItemPreviewFormatHttpResponse } from './item-preview-format-http-response';
 export { ItemPreviewHttpResponse } from './item-preview-http-response';
 export { AboutSectionHttpResponse } from './about-section-http-response';
+export {AboutSectionFormatHttpResponse} from './about-section-format-http-response';
