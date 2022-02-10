da58928ddcebfeb26a409c1313f525d10b5321ed
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 15:42:35 +0300

    - Create method 'listBannerAds$'.

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { retryWithBackoff } from '@sharedModule/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,14 @@ export class HomeService {
   constructor(
     private _httpClient: HttpClient
   ) { }
+
+  listBannerAds$() {
+    const api = environment.baseURL +
+      environment.home.rootURL +
+      environment.home.bannerAd.listBannerAd();
+
+    return this._httpClient.get(
+      api).pipe(
+        retryWithBackoff(1000, 5));
+  }
 }
