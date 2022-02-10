f69cbeb4b4ee5167d39aa992152418808b35eb29
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 16:20:39 +0300

    - Export function 'isObjectEmpty'.

--- src/app/shared/utilities/index.ts
+++ src/app/shared/utilities/index.ts
@@ -2,3 +2,4 @@ export { convertItemToNumeric } from './convert-item-to-numeric.util';
 export { convertItemToString } from './convert-item-to-string.util';
 export { getBoolean } from './get-boolean.util';
 export { isANumber } from './is-a-number.util';
+export { isObjectEmpty } from './is-object-empty.util';
