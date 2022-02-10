adcac2eab073ec8b0f00aff463c92ac5394eff8c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 16:26:37 +0300

    - Update function 'retryWithBackoff'.

--- src/app/shared/operators/retry-with-back-off.ts
+++ src/app/shared/operators/retry-with-back-off.ts
@@ -7,10 +7,10 @@ const getErrorMessage = (maxRetry: number) =>
 const DEFAULT_MAX_RETRIES = 5;
 const DEFAULT_BACKOFF = 1000;
 
-export function retryWithBackoff(delayMs: number, maxRetry = DEFAULT_MAX_RETRIES, backoffMs = DEFAULT_BACKOFF) {
+export function retryWithBackoff<T>(delayMs: number, maxRetry = DEFAULT_MAX_RETRIES, backoffMs = DEFAULT_BACKOFF) {
     let retries = maxRetry;
 
-    return (src: Observable<any>) =>
+    return (src: Observable<T>) =>
         src.pipe(
             retryWhen((errors: Observable<any>) => errors.pipe(
                 mergeMap(error => {
