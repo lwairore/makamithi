b09dc7248a47edca1dcf4c250a2aa1951a3c681e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 16:20:17 +0300

    - Export function 'isANumber'.

--- src/app/shared/utilities/index.ts
+++ src/app/shared/utilities/index.ts
@@ -1,3 +1,4 @@
 export { convertItemToNumeric } from './convert-item-to-numeric.util';
 export { convertItemToString } from './convert-item-to-string.util';
 export { getBoolean } from './get-boolean.util';
+export { isANumber } from './is-a-number.util';
