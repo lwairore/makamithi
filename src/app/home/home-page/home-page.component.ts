54f513bb6e4a9adddb7e98a74ed4faa5e308be41
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 15:43:25 +0300

    - Register service 'HomeService' as a root provider.

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../home.service';
 
 @Component({
   selector: 'mak-pit-home-page',
   templateUrl: './home-page.component.html',
   styles: [
+  ],
+  providers: [
+    HomeService,
   ]
 })
 export class HomePageComponent implements OnInit {
