8c06c77103a8f194840d52e510bebf6e257b9f39
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 09:50:21 +0300

    - Update object 'listOurFeature'.

--- src/app/home/home-page/sections/h1-features-section/h1-features-section.component.ts
+++ src/app/home/home-page/sections/h1-features-section/h1-features-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Immutable from 'immutable';
 
 @Component({
   selector: 'mak-pit-h1-features-section',
@@ -7,33 +8,7 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class H1FeaturesSectionComponent implements OnInit {
-  listOurFeature = [
-    {
-      image: 'assets/img/features/1.png',
-      title: 'Technical support from our agronomists',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
-    },
-    {
-      image: 'assets/img/features/2.png',
-      title: 'Consultation',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
-    },
-    {
-      image: 'assets/img/features/3.png',
-      title: 'Farmers training',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
-    },
-    {
-      image: 'assets/img/features/1.png',
-      title: 'Policy implementation',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
-    },
-    {
-      image: 'assets/img/features/2.png',
-      title: 'Business advisory',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
-    },
-  ]
+  listOurFeature = Immutable.fromJS([]);
 
   constructor() { }
 
