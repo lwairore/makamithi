2ac3bb48a4ed22306c7ad186e792cec5eb30ecc5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 14:32:41 +0300

    - Add optional property 'photo'.

--- src/app/home/custom-types/feature-section-http-response.ts
+++ src/app/home/custom-types/feature-section-http-response.ts
@@ -1,4 +1,7 @@
+import { ItemPreviewHttpResponse } from "./item-preview-http-response";
+
 export type FeatureSectionHttpResponse = Readonly<{
     heading?: string;
     description?: string;
+    photo?: ItemPreviewHttpResponse;
 }>
