076ae79e260ea277487d8a663bd2d2660a164fa7
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 16:14:26 +0300

    - Remove property 'heading'.

--- src/app/home/custom-types/feature-section-http-response.ts
+++ src/app/home/custom-types/feature-section-http-response.ts
@@ -1,7 +1,6 @@
 import { ItemPreviewHttpResponse } from "./item-preview-http-response";
 
 export type FeatureSectionHttpResponse = Readonly<{
-    heading?: string;
     description?: string;
     photo?: ItemPreviewHttpResponse;
 }>
