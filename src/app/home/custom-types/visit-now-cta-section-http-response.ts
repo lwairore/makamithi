102882e0a30a990fa044f41216d59950adec75f5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 14:06:56 +0300

    - Add 'optional' property 'section_image' on type 'VisitNowCtaSectionHttpResponse'.

--- src/app/home/custom-types/visit-now-cta-section-http-response.ts
+++ src/app/home/custom-types/visit-now-cta-section-http-response.ts
@@ -4,4 +4,5 @@ export type VisitNowCtaSectionHttpResponse = Readonly<{
     heading?: string;
     description?: string;
     background_image?: ItemPreviewHttpResponse;
+    section_image?: ItemPreviewHttpResponse;
 }>
