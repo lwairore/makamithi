cd63a03fc541229723fc78d33fcf60caf50cd4fb
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 10:54:27 +0300

    - Add property 'photo'.

--- src/app/home/custom-types/product-format-http-response.ts
+++ src/app/home/custom-types/product-format-http-response.ts
@@ -1,4 +1,7 @@
+import { ItemPreviewFormatHttpResponse } from ".";
+
 export type ProductFormatHttpResponse = Readonly<{
     title: string;
     id: number;
+    photo: ItemPreviewFormatHttpResponse;
 }>
