c302caaa86e10476c1082197fce93c6abeab139f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 15:57:55 +0300

    - Add property 'sectionImage' on type 'WhyChooseUsSectionFormatHttpResponse'.

--- src/app/home/custom-types/why-choose-us-section-format-http-response.ts
+++ src/app/home/custom-types/why-choose-us-section-format-http-response.ts
@@ -1,4 +1,7 @@
+import { ItemPreviewFormatHttpResponse } from "./item-preview-format-http-response";
+
 export type WhyChooseUsSectionFormatHttpResponse = Readonly<{
     heading: string;
     description: string;
+    sectionImage: ItemPreviewFormatHttpResponse;
 }>
