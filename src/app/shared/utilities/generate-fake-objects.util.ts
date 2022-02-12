157bb98776ca6959eb3e98d7e96742320bba485a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 11:29:19 +0300

    - Set default value for 'count' to '8'.

--- src/app/shared/utilities/generate-fake-objects.util.ts
+++ src/app/shared/utilities/generate-fake-objects.util.ts
@@ -1,7 +1,7 @@
 import { memoize } from "lodash";
 
 // Generate Fake Object Array
-export const generateFakeObjectArray = memoize((count: number) => {
+export const generateFakeObjectArray = memoize((count = 8) => {
     const indexes: Array<number> = [];
     for (let i = 0; i < count; i++) {
         indexes.push(i);
