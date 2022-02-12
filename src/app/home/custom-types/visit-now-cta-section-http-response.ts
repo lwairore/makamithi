5031d72787789a93c5bbb7d7e9f3b26d06bff51a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 14:06:43 +0300

    - Add 'optional' property 'background_image' on type 'VisitNowCtaSectionHttpResponse'.

--- src/app/home/custom-types/visit-now-cta-section-http-response.ts
+++ src/app/home/custom-types/visit-now-cta-section-http-response.ts
@@ -1,4 +1,7 @@
+import { ItemPreviewHttpResponse } from "./item-preview-http-response";
+
 export type VisitNowCtaSectionHttpResponse = Readonly<{
     heading?: string;
     description?: string;
+    background_image?: ItemPreviewHttpResponse;
 }>
