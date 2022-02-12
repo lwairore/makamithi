53f3cfcc825133c346a21cfcb4644118f05a1f80
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 15:56:08 +0300

    - Add 'optional' property 'section_image' on 'type' 'WhyChooseUsSectionHttpResponse'.

--- src/app/home/custom-types/why-choose-us-section-http-response.ts
+++ src/app/home/custom-types/why-choose-us-section-http-response.ts
@@ -1,4 +1,7 @@
+import { ItemPreviewHttpResponse } from "./item-preview-http-response";
+
 export type WhyChooseUsSectionHttpResponse = Readonly<{
     heading?: string;
     description?: string;
+    section_image?: ItemPreviewHttpResponse;
 }>
