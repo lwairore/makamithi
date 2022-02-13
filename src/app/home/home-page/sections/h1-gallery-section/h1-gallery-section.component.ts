17c74b1fa4f5a3630550eabea52831454077b958
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 15:03:07 +0300

    - Change 'changeDetection' strategy to 'OnPush'.

--- src/app/home/home-page/sections/h1-gallery-section/h1-gallery-section.component.ts
+++ src/app/home/home-page/sections/h1-gallery-section/h1-gallery-section.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mak-pit-h1-gallery-section',
   templateUrl: './h1-gallery-section.component.html',
   styles: [
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class H1GallerySectionComponent implements OnInit {
 
