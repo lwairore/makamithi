e58edc9d59431c73ecf6621e1b206e7446fdb04b
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 15:04:45 +0300

    - Create method '_manuallyTriggerChangeDetection'.

--- src/app/home/home-page/sections/h1-gallery-section/h1-gallery-section.component.ts
+++ src/app/home/home-page/sections/h1-gallery-section/h1-gallery-section.component.ts
@@ -18,4 +18,7 @@ export class H1GallerySectionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  private _manuallyTriggerChangeDetection() {
+    this._changeDetectorRef.detectChanges();
+  }
 }
