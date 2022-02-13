0b5af9e091e76f42e4f82e68470849aac5966aed
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 14:02:52 +0300

    - Add 'optional' property 'image' on type 'CoreValueHttpResponse'.

--- src/app/home/custom-types/core-value-http-response.ts
+++ src/app/home/custom-types/core-value-http-response.ts
@@ -1,4 +1,4 @@
-import { ItemPreviewHttpResponse } from ".";
+import { ItemPreviewHttpResponse } from "./item-preview-http-response";
 
 export type CoreValueHttpResponse = Readonly<{
     title?: string;
