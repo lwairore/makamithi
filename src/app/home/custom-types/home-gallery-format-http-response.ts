31128d8cf95cf3579ae6c2355a1858e1f7d8bb78
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 20:25:53 +0300

    - Add property 'image' on type 'HomeGalleryFormatHttpResponse'.

--- src/app/home/custom-types/home-gallery-format-http-response.ts
+++ src/app/home/custom-types/home-gallery-format-http-response.ts
@@ -1,2 +1,5 @@
+import { ItemPreviewFormatHttpResponse } from "./item-preview-format-http-response";
+
 export type HomeGalleryFormatHttpResponse = Readonly<{
+    image: ItemPreviewFormatHttpResponse;
 }>
