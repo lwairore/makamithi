212bebf9802b905db639e302e0281e56250e69d6
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 15:20:08 +0300

    - Add 'optional' property 'background_image' on type 'GallerySectionHttpResponse'.

--- src/app/home/custom-types/gallery-section-http-response.ts
+++ src/app/home/custom-types/gallery-section-http-response.ts
@@ -4,4 +4,5 @@ export type GallerySectionHttpResponse = Readonly<{
     heading?: string;
     summary?: string;
     section_image?: ItemPreviewHttpResponse;
+    background_image?: ItemPreviewHttpResponse;
 }>
