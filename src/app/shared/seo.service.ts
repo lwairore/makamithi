948555d5307904e04ae789b6cd24088ca5bb7fb1
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 11:44:25 +0300

    - Create method 'retrieveHomeSEODetails$'.

--- src/app/shared/seo.service.ts
+++ src/app/shared/seo.service.ts
@@ -4,6 +4,8 @@ import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { SeoDetailsHttpResponse } from './custom-types';
 import { retryWithBackoff } from './operators';
+import { convertItemToString } from './utilities';
+import { ExpectedType, whichValueShouldIUse } from './utilities/which-value-should-i-use.util';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +20,27 @@ export class SeoService {
 
     return this._httpClient.get<SeoDetailsHttpResponse>(API)
       .pipe(
-        retryWithBackoff(1000, 5))
+        retryWithBackoff(1000, 5),
+        map(details => {
+          const formattedDetails = {
+            title: convertItemToString(details.title),
+            keywords: convertItemToString(details.keywords),
+            description: convertItemToString(details.description),
+            image: {
+              width: whichValueShouldIUse(details.image?.width, null, ExpectedType.NUMBER),
+              height: whichValueShouldIUse(details.image?.height, null, ExpectedType.NUMBER),
+              src: convertItemToString(details.image?.image),
+              alt: convertItemToString(details.image?.caption),
+            },
+            type: convertItemToString(details.type),
+            author: convertItemToString(details.author),
+            section: convertItemToString(details.section),
+            published: convertItemToString(details.published),
+            modified: convertItemToString(details.modified),
+          }
+
+          return formattedDetails;
+        })
+      )
   }
 }
