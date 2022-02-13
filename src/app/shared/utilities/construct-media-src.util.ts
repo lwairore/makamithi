caf471470dfcecd8dc0326856bb67fd6d2e994a9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 11:34:18 +0300

    - Create method 'constructMediaSrc'.

---
+++ src/app/shared/utilities/construct-media-src.util.ts
@@ -0,0 +1,15 @@
+import { memoize } from "lodash-es";
+import { environment } from "src/environments/environment";
+import { convertItemToString } from "./convert-item-to-string.util";
+import { stringIsEmpty } from "./string-is-empty.util";
+
+export const constructMediaSrc = memoize((src: any) => {
+    if (stringIsEmpty(src)) { return ''; }
+
+    const convertSrcToString = convertItemToString(src);
+
+    const newSrc = (environment.production)
+        ? convertSrcToString : `${environment.imageBaseURL}${convertSrcToString}`;
+
+    return newSrc;
+});
