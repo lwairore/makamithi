c2f0c1a24fc54b568ae5784a5c83eff870801eca
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 11:32:35 +0300

    - Initial commit.

--- src/app/shared/seo.service.ts
+++ src/app/shared/seo.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { SeoDetailsHttpResponse } from './custom-types';
+import { retryWithBackoff } from './operators';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +11,13 @@ import { Injectable } from '@angular/core';
 export class SeoService {
 
   constructor(private _httpClient: HttpClient) { }
+
+  retrieveHomeSEODetails$() {
+    const API = environment.home.rootURL
+      + environment.home.seo;
+
+    return this._httpClient.get<SeoDetailsHttpResponse>(API)
+      .pipe(
+        retryWithBackoff(1000, 5))
+  }
 }
