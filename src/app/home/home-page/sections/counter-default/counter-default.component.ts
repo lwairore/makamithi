3ee6486ef9c2c9d4c6aea6611494d3fc216f8f9d
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-02-13 10:49:53 +0300

    Define count animation

--- src/app/home/home-page/sections/counter-default/counter-default.component.ts
+++ src/app/home/home-page/sections/counter-default/counter-default.component.ts
@@ -7,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class CounterDefaultComponent implements OnInit {
+  yearsOfExperienceCount: number = 0;
+  expertMemberCount: number = 0;
+  winAwardsCount:number = 0;
+  worldWideBranchCount: any = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  yearsOfExperienceCountStop:any = setInterval(()=>{
+    this.yearsOfExperienceCount++;
+
+    if(this.yearsOfExperienceCount == 25){
+      clearInterval(this.yearsOfExperienceCountStop)
+    }
+  },100)
+
+
+  expertMemberCountStop:any = setInterval(()=>{
+    this.expertMemberCount++;
+
+    if(this.expertMemberCount == 98){
+      clearInterval(this.expertMemberCountStop)
+    }
+  },100)
+
+  winAwardsCountStop:any = setInterval(()=>{
+    this.winAwardsCount++;
+
+    if(this.winAwardsCount == 49){
+      clearInterval(this.winAwardsCountStop)
+    }
+  },100)
+
+  worldWideBranchCountStop:any = setInterval(()=>{
+    this.worldWideBranchCount++;
+
+    if(this.worldWideBranchCount == 85){
+      clearInterval(this.winAwardsCountStop)
+    }
+  },100)
 
 }
