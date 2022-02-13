1f7b135b3b0429319fc5c244c2a074a6f2c63859
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 11:34:52 +0300

    - Export function 'constructMediaSrc'.

--- src/app/shared/utilities/index.ts
+++ src/app/shared/utilities/index.ts
@@ -4,3 +4,4 @@ export { getBoolean } from './get-boolean.util';
 export { isANumber } from './is-a-number.util';
 export { isObjectEmpty } from './is-object-empty.util';
 export { stringIsEmpty } from './string-is-empty.util';
+export { constructMediaSrc } from './construct-media-src.util';
