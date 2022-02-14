d60f2cb40d1f5a93b0badabda5c97c0c02b99cb3
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:38:25 +0300

    - Define method '_formatShowcaseItemWithPhoto'.

--- src/app/about-us/about-us.service.ts
+++ src/app/about-us/about-us.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ItemPreviewFormatHttpResponse, ItemPreviewHttpResponse } from '@sharedModule/custom-types';
+import { constructMediaSrc, convertItemToString } from '@sharedModule/utilities';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,14 @@ export class AboutUsService {
   constructor(
     private _httpClient: HttpClient,
   ) { }
+
+  private _formatShowcaseItemWithPhoto(
+    photo?: ItemPreviewHttpResponse) {
+    const formattedPhoto: ItemPreviewFormatHttpResponse = {
+      alt: convertItemToString(photo?.caption),
+      src: constructMediaSrc(photo?.image),
+    }
+
+    return formattedPhoto;
+  }
 }
