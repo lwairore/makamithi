85a7d0e4a39db6c8583894e0b3f1bf0319d395a1
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:43:48 +0300

    - Add 'optional' property 'section_image' on type 'ApAboutSectionHttpResponse'.

--- src/app/about-us/custom-types/ap-about-section-http-response.ts
+++ src/app/about-us/custom-types/ap-about-section-http-response.ts
@@ -1,5 +1,8 @@
+import { ItemPreviewHttpResponse } from "@sharedModule/custom-types";
+
 export interface ApAboutSectionHttpResponse {
     heading?: string;
     subheading?: string;
     description?: string;
+    section_image?: ItemPreviewHttpResponse;
 }
