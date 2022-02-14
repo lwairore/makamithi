b1ac5d53f82d79e34a313ae4a7c3d6d734f33e4f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:34:53 +0300

    - Inject 'HttpClient'.

--- src/app/about-us/about-us.service.ts
+++ src/app/about-us/about-us.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class AboutUsService {
 
-  constructor() { }
+  constructor(
+    private _httpClient: HttpClient,
+  ) { }
 }
