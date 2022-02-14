10a4e55ce2edd8e125f7fc045da7f5a840e457ce
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 13:02:52 +0300

    - Initialize subscription object '_retrieveAboutUsSEODetailsSubscription'.

--- src/app/about-us/about-main/about-main.component.ts
+++ src/app/about-us/about-main/about-main.component.ts
@@ -2,6 +2,7 @@ import { Location } from '@angular/common';
 import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { SeoService } from '@sharedModule/services/seo.service';
 import { SeoSocialShareService } from 'ngx-seo';
+import { Subscription } from 'rxjs';
 import { AboutUsService } from '../about-us.service';
 
 @Component({
@@ -15,6 +16,7 @@ import { AboutUsService } from '../about-us.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutMainComponent implements OnInit, AfterViewInit {
+  private _retrieveAboutUsSEODetailsSubscription: Subscription | undefined;
 
   constructor(
     private _seoService: SeoService,
