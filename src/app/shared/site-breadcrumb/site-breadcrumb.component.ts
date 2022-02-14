0896522889c01ece6486e17158aa9ff54d6c12d9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 08:38:53 +0300

    - Initialize subscription object '_retrieveSiteBreadcrumbDetailsSubscription'.

--- src/app/shared/site-breadcrumb/site-breadcrumb.component.ts
+++ src/app/shared/site-breadcrumb/site-breadcrumb.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import * as Immutable from 'immutable';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'mak-pit-site-breadcrumb',
@@ -12,6 +13,8 @@ export class SiteBreadcrumbComponent implements OnInit {
   @Input() pageTitle = 'Blog';
 
   siteBreadcrumbDetails = Immutable.fromJS({});
+
+  private _retrieveSiteBreadcrumbDetailsSubscription: Subscription | undefined;
 
   constructor(
     private _changeDetectorRef: ChangeDetectorRef,
