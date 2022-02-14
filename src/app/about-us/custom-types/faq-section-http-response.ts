4bc79bb41021caed40d62ba7aa67b3ba1ddd1e4f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 14:23:27 +0300

    - Add 'optional' property 'background_image' on type 'FaqSectionHttpResponse'.

--- src/app/about-us/custom-types/faq-section-http-response.ts
+++ src/app/about-us/custom-types/faq-section-http-response.ts
@@ -1,3 +1,6 @@
+import { ItemPreviewHttpResponse } from "@sharedModule/custom-types";
+
 export type FaqSectionHttpResponse = Readonly<{
     title?: string;
+    background_image?: ItemPreviewHttpResponse;
 }>
