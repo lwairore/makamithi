dd0e99d59b03b748830af442604ee5883d8666b5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:46:04 +0300

    - Add property 'sectionImage' on type 'ApAboutSectionFormatHttpResponse'.

--- src/app/about-us/custom-types/ap-about-section-format-http-response.ts
+++ src/app/about-us/custom-types/ap-about-section-format-http-response.ts
@@ -1,5 +1,8 @@
+import { ItemPreviewFormatHttpResponse } from "@sharedModule/custom-types";
+
 export type ApAboutSectionFormatHttpResponse = Readonly<{
     heading: string;
     subheading: string;
     description: string;
+    sectionImage: ItemPreviewFormatHttpResponse;
 }>
