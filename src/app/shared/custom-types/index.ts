10c286dfa293491a1d17078e3b0f9340254a0b1a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 08:52:25 +0300

    - Export type 'ItemPreviewHttpResponse'.

--- src/app/shared/custom-types/index.ts
+++ src/app/shared/custom-types/index.ts
@@ -1,2 +1,3 @@
 export { ImageAuxDataHttpResponse } from './image-aux-data-http-response';
 export { SeoDetailsHttpResponse } from './seo-details-http-response';
+export { ItemPreviewHttpResponse } from './item-preview-http-response';
