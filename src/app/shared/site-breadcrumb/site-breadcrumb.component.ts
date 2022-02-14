f47d4750d1e3ca521bf4dd411ab2e0358153a163
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 08:30:40 +0300

    - Change the 'changeDetection' to 'OnPush'.

--- src/app/shared/site-breadcrumb/site-breadcrumb.component.ts
+++ src/app/shared/site-breadcrumb/site-breadcrumb.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mak-pit-site-breadcrumb',
   templateUrl: './site-breadcrumb.component.html',
   styles: [
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SiteBreadcrumbComponent implements OnInit {
   @Input() pageTitle = 'Blog';
