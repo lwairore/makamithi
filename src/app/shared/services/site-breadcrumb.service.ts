90053d3be4f93157e4ace8bf31773e9e912a59a6
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 09:01:11 +0300

    - Create method 'retrieveSiteBreadcrumbDetails$'.

--- src/app/shared/services/site-breadcrumb.service.ts
+++ src/app/shared/services/site-breadcrumb.service.ts
@@ -1,5 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SiteBreadcrumbDetailsFormatHttpResponse } from '@sharedModule/custom-types/site-breadcrumb-details-format-http-response';
+import { SiteBreadcrumbDetailsHttpResponse } from '@sharedModule/custom-types/site-breadcrumb-details-http-response';
+import { retryWithBackoff } from '@sharedModule/operators';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +14,22 @@ export class SiteBreadcrumbService {
   constructor(
     private _httpClient: HttpClient,
   ) { }
+
+  retrieveSiteBreadcrumbDetails$() {
+    const API = environment.baseURL +
+      environment.siteBreadcrumb.rootURL +
+      environment.siteBreadcrumb.retrieveSiteBreadcrumbDetails;
+
+    return this._httpClient.get<SiteBreadcrumbDetailsHttpResponse>(API)
+      .pipe(
+        retryWithBackoff(1000, 5),
+        map(details => {
+          const FORMATTED_DETAILS: SiteBreadcrumbDetailsFormatHttpResponse = {
+            backgroundImage: this._formatShowcaseItemWithPhoto(details.background_image),
+          }
+
+          return FORMATTED_DETAILS;
+        })
+      )
+  }
 }
