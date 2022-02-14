fffbef6f658f35c6cc497d2d500a9d8559b8d973
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:57:23 +0300

    - Register service 'AboutUsService' as a 'provider'.

--- src/app/about-us/about-main/about-main.component.ts
+++ src/app/about-us/about-main/about-main.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AboutUsService } from '../about-us.service';
 
 @Component({
   selector: 'mak-pit-about-main',
   templateUrl: './about-main.component.html',
   styles: [
+  ],
+  providers: [
+    AboutUsService,
   ]
 })
 export class AboutMainComponent implements OnInit {
