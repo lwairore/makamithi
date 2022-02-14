5cb013c9732c210f3cd5fbab3ca18d971e8d67d5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:51:09 +0300

    - Change the 'changeDetection' strategy to 'OnPush'.

--- src/app/about-us/about-main/sections/apabout4-section/apabout4-section.component.ts
+++ src/app/about-us/about-main/sections/apabout4-section/apabout4-section.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mak-pit-apabout4-section',
   templateUrl: './apabout4-section.component.html',
   styles: [
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class APAbout4SectionComponent implements OnInit {
 
