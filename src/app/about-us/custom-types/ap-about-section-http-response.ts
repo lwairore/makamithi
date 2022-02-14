4ca7affdede2a181bdadbb92563faacbdec7159a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:44:35 +0300

    - Make properties for type 'ApAboutSectionHttpResponse' to be 'immutable'.

--- src/app/about-us/custom-types/ap-about-section-http-response.ts
+++ src/app/about-us/custom-types/ap-about-section-http-response.ts
@@ -1,8 +1,8 @@
 import { ItemPreviewHttpResponse } from "@sharedModule/custom-types";
 
-export type ApAboutSectionHttpResponse = {
+export type ApAboutSectionHttpResponse = Readonly<{
     heading?: string;
     subheading?: string;
     description?: string;
     section_image?: ItemPreviewHttpResponse;
-}
+}>
