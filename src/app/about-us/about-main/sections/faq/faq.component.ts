6b0dbb3f4ffc81ec6bfc96e2ae693612cc740c17
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 15:52:47 +0300

    - Change the data type of object 'faqs'.

--- src/app/about-us/about-main/sections/faq/faq.component.ts
+++ src/app/about-us/about-main/sections/faq/faq.component.ts
@@ -18,20 +18,7 @@ export class FaqComponent implements OnInit, AfterViewInit, OnDestroy {
 
   private _loadRequiredDetailsSubscription: Subscription | undefined;
 
-  faqs = [
-    {
-      title: 'Why Do You Eat Orange Food?',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat aute irure aliquam quaerat.'
-    },
-    {
-      title: 'Why Orange Food Is The Best For Health?',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat aute irure aliquam quaerat.'
-    },
-    {
-      title: 'Good Food For Good Health?',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat aute irure aliquam quaerat.'
-    },
-  ]
+  faqs = Immutable.fromJS([]);
 
   constructor(
     private _aboutUsService: AboutUsService,
