934bf150c096c10ef28a54f82bd1f619da254f90
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 11:38:11 +0300

    - Update method '_loadRequiredDetails'.

--- src/app/about-us/about-main/sections/faq/faq.component.ts
+++ src/app/about-us/about-main/sections/faq/faq.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import * as Immutable from 'immutable';
+import { forkJoin, Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AboutUsService } from '../../../about-us.service';
 
 @Component({
@@ -39,7 +41,7 @@ export class FaqComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     this._loadRequiredDetails();
-   }
+  }
 
   ngOnDestroy(): void {
     this._unsubscribeLoadRequiredDetailsSubscription();
@@ -56,6 +58,17 @@ export class FaqComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   private _loadRequiredDetails() {
+    const FAQ_SECTION_DETAILS$ = this._aboutUsService
+      .retrieveFaqSection$();
+
+    this._loadRequiredDetailsSubscription = forkJoin([
+      FAQ_SECTION_DETAILS$.pipe(
+        tap(details => {
+          this.faqSectionDetails = Immutable.fromJS(details);
+        })),
+    ])
+      .subscribe(_ => {
+      }, err => console.error(err))
 
   }
 }
