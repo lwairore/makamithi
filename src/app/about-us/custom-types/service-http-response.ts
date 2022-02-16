e630a2c53e11bf1cb53782424d00696220444c32
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 17:24:03 +0300

    - Add 'optional' property 'title' on type 'ServiceHttpResponse'.

--- src/app/about-us/custom-types/service-http-response.ts
+++ src/app/about-us/custom-types/service-http-response.ts
@@ -3,4 +3,5 @@ import { ItemPreviewHttpResponse } from "@sharedModule/custom-types";
 export type ServiceHttpResponse = Readonly<{
     about_photo?: ItemPreviewHttpResponse;
     id?: number;
+    title?: string;
 }>
