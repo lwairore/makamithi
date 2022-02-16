579cc9235db772d24f49d2ea3d8eae49add2446d
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 18:06:29 +0300

    - Add 'optional' property 'section_image' on type 'WhatWeDoSectionHttpResponse'.

--- src/app/about-us/custom-types/what-we-do-section-http-response.ts
+++ src/app/about-us/custom-types/what-we-do-section-http-response.ts
@@ -1,4 +1,7 @@
+import { ItemPreviewHttpResponse } from "@sharedModule/custom-types";
+
 export type WhatWeDoSectionHttpResponse = Readonly<{
     heading?: string;
     summary?: string;
+    section_image?: ItemPreviewHttpResponse;
 }>
