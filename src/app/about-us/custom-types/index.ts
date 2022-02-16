11fff46b4db07100b1bf6036300669a5c1bf89cb
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 15:46:24 +0300

    - Export type 'FaqHttpResponse'.

--- src/app/about-us/custom-types/index.ts
+++ src/app/about-us/custom-types/index.ts
@@ -2,3 +2,4 @@ export { ApAboutSectionFormatHttpResponse } from './ap-about-section-format-http
 export { ApAboutSectionHttpResponse } from './ap-about-section-http-response';
 export { FaqSectionHttpResponse } from './faq-section-http-response';
 export { FaqSectionFormatHttpResponse } from './faq-section-format-http-response';
+export { FaqHttpResponse } from './faq-http-response';
