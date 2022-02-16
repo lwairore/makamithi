cfc87addc6c8135dec55b53070f3e34f48f7d6b2
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 17:14:01 +0300

    - Add 'optional' property 'id' on type 'ServiceHttpResponse'.

--- src/app/about-us/custom-types/service-http-response.ts
+++ src/app/about-us/custom-types/service-http-response.ts
@@ -2,4 +2,5 @@ import { ItemPreviewHttpResponse } from "@sharedModule/custom-types";
 
 export type ServiceHttpResponse = Readonly<{
     about_photo?: ItemPreviewHttpResponse;
+    id?: number;
 }>
