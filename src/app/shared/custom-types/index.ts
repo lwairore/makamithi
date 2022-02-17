36f7cc92a6a5c7550f1a6b94610c97642b481977
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 17:13:20 +0300

    - Export type 'AreaSectionHttpResponse'.

--- src/app/shared/custom-types/index.ts
+++ src/app/shared/custom-types/index.ts
@@ -3,3 +3,4 @@ export { SeoDetailsHttpResponse } from './seo-details-http-response';
 export { ItemPreviewHttpResponse } from './item-preview-http-response';
 export { ItemPreviewFormatHttpResponse } from './item-preview-format-http-response';
 export { AreaSectionFormatHttpResponse } from './area-section-format-http-response';
+export { AreaSectionHttpResponse } from './area-section-http-response';
