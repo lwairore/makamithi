78f4e808bb47c7a1dc10708654f98ba490dbf892
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 11:19:34 +0300

    - Update the data type of object 'listService'.

--- src/app/about-us/about-main/sections/ap-what-we-do2-section/ap-what-we-do2-section.component.ts
+++ src/app/about-us/about-main/sections/ap-what-we-do2-section/ap-what-we-do2-section.component.ts
@@ -12,33 +12,7 @@ import { AboutUsService } from 'src/app/about-us/about-us.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ApWhatWeDo2SectionComponent implements OnInit, AfterViewInit, OnDestroy {
-  listService = [
-    {
-      image: 'assets/img/icon/icon1.png',
-      title: 'Technical support from our agronomists',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
-    },
-    {
-      image: 'assets/img/icon/icon2.png',
-      title: 'Consultation',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
-    },
-    {
-      image: 'assets/img/icon/icon3.png',
-      title: 'Farmers training',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
-    },
-    {
-      image: 'assets/img/icon/icon1.png',
-      title: 'Policy implementation',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
-    },
-    {
-      title: 'Business advisory',
-      image: 'assets/img/icon/icon2.png',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt ut labore et dolore magna aliqua.'
-    },
-  ]
+  listService = Immutable.fromJS([]);
 
   whatWeDoSectionDetails = Immutable.fromJS({});
 
