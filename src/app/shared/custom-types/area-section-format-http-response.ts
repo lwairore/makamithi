27399617fc219db7f281f6d5d111a4a54f0d0fd5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 17:12:32 +0300

    - Add property 'sectionImage' on type 'AreaSectionFormatHttpResponse'.

--- src/app/shared/custom-types/area-section-format-http-response.ts
+++ src/app/shared/custom-types/area-section-format-http-response.ts
@@ -1,4 +1,7 @@
+import { ItemPreviewFormatHttpResponse } from "./item-preview-format-http-response";
+
 export type AreaSectionFormatHttpResponse = Readonly<{
     heading: string;
     summary: string;
+    sectionImage: ItemPreviewFormatHttpResponse;
 }>
