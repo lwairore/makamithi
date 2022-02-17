5dc5412c392a3c8ee95c7d941c8b988e7b847315
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 17:10:09 +0300

    - Add optional property 'section_image' on type 'AreaSectionHttpResponse'.

--- src/app/shared/custom-types/area-section-http-response.ts
+++ src/app/shared/custom-types/area-section-http-response.ts
@@ -1,4 +1,7 @@
+import { ItemPreviewHttpResponse } from "./item-preview-http-response";
+
 export type AreaSectionHttpResponse = Readonly<{
     heading?: string;
     summary?: string;
+    section_image?: ItemPreviewHttpResponse;
 }>
