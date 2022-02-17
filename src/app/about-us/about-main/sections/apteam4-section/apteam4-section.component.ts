169f3771d5838db2270f4befa42bf0dbdcb8e93a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 17:26:49 +0300

    - Change the 'changeDetection' strategy to 'OnPush'.

--- src/app/about-us/about-main/sections/apteam4-section/apteam4-section.component.ts
+++ src/app/about-us/about-main/sections/apteam4-section/apteam4-section.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mak-pit-apteam4-section',
   templateUrl: './apteam4-section.component.html',
   styles: [
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class APTeam4SectionComponent implements OnInit {
 
