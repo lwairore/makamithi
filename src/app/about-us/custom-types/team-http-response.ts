30cdb09812feb99a03e0e6f3004fb80cc5debaec
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-20 18:00:24 +0300

    - Update type 'TeamHttpResponse'.

--- src/app/about-us/custom-types/team-http-response.ts
+++ src/app/about-us/custom-types/team-http-response.ts
@@ -1,2 +1,9 @@
-export interface TeamHttpResponse {
-}
+import { ItemPreviewHttpResponse } from "@sharedModule/custom-types";
+
+export type TeamHttpResponse = Readonly<{
+    full_name?: string;
+    role?: string;
+    image?: ItemPreviewHttpResponse;
+    facebook?: string;
+    twitter?: string;
+}>
