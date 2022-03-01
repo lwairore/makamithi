7d2a570262b2e9fbb6fa868cb2e9884f2a25ce8f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-03-01 10:38:10 +0300

    - Export types.

---
+++ src/app/services/custom-types/index.ts
@@ -0,0 +1,9 @@
+export { ServiceFormatHttpResponse } from './service-format-http-response';
+export { ServiceHttpResponse } from './service-http-response';
+export { ServiceAreaSectionFormatHttpResponse } from './service-area-section-format-http-response';
+export { ServiceAreaSectionHttpResponse } from './service-area-section-http-response';
+export { VideoFormatHttpResponse } from './video-format-http-response';
+export { VideoHttpResponse } from './video-http-response';
+export { ServiceSidebarHttpResponse } from './service-sidebar-http-response';
+export { ServiceDetailFormatHttpResponse } from './service-detail-format-http-response';
+export { ServiceDetailHttpResponse } from './service-detail-http-response';
