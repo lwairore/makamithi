84c19483b05dc82e4e4bde2ef6401939235ad62a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-03-01 10:34:35 +0300

    - Remove type 'ProductHttpResponse'.

--- src/app/home/custom-types/product-http-response.ts
+++
@@ -1,8 +0,0 @@
-import { ItemPreviewHttpResponse } from "@sharedModule/custom-types";
-
-export type ProductHttpResponse = Readonly<{
-    title?: string;
-    id?: number;
-    photo?: ItemPreviewHttpResponse;
-    price?: number;
-}>
