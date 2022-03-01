768e984021a7ef459e4f58497f8c1f4e1d17a18f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-03-01 10:36:14 +0300

    - Export types.

---
+++ src/app/gallery/custom-types/index.ts
@@ -0,0 +1,4 @@
+export { GalleryFormatHttpResponse } from './gallery-format-http-response';
+export { GalleryHttpResponse } from './gallery-http-response';
+export { GalleryDetailHttpResponse } from './gallery-detail-http-response';
+export { GalleryDetailFormatHttpResponse } from './gallery-detail-format-http-response';
