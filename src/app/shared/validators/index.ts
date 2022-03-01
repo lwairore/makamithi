eff62d12822f5c5bd88be4f7e7da7bf753193ac1
Lucas Wairore Karangu <kwairore@gmail.com>
2022-03-01 10:44:28 +0300

    - Export needed artifacts.

---
+++ src/app/shared/validators/index.ts
@@ -0,0 +1,2 @@
+export { MinCharacterNotGenuinelyAchievedValidator } from './min-character-not-genuinely-achieved.validator';
+export { NoValueProvidedValidator } from './no-value-provided.validator';
