ace7c439318452ad12650893d41677232ed67b4e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-03-01 10:45:55 +0300

    - Export needed types.

---
+++ src/app/shop/custom-types/index.ts
@@ -0,0 +1,4 @@
+export { ProductDetailHttpResponse } from './product-detail-http-response';
+export { ProductDetailFormatHttpResponse } from './product-detail-format-http-response';
+export { ProductReviewHttpResponse } from './product-review-http-response';
+export { ProductReviewFormatHttpResponse } from './product-review-format-http-response';
