d2fd23e20bb43fd9d2ea934ee1c49dd6657bb11d
Lucas Wairore Karangu <kwairore@gmail.com>
2022-03-01 10:44:44 +0300

    - Create class 'MinCharacterNotGenuinelyAchievedValidator'.

---
+++ src/app/shared/validators/min-character-not-genuinely-achieved.validator.ts
@@ -0,0 +1,26 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { convertItemToString, stringIsEmpty } from '@sharedModule/utilities';
+
+export class MinCharacterNotGenuinelyAchievedValidator {
+    static minCharacterNotGenuinelyAchieved(minCharacters: number = 2): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } | null => {
+            const controlValue = convertItemToString(
+                control.value).trim();
+            if (stringIsEmpty(controlValue) && !control.touched)
+                // if control is empty return no error
+                return null;
+            // const regexExpression = new RegExp(`/[a-z,.'-]{${minCharacters},}/`, 'i');
+            // const regexExpression = new RegExp(`/[a-z]/`, 'gi');
+            // console.log(regexExpression.test(controlValue))
+            let valid: boolean;
+            try {
+                valid = controlValue.length >= minCharacters;
+            } catch (TypeError) {
+                valid = false;
+            }
+
+
+            return valid ? null : { minCharacterNotGenuinelyAchieved: true }
+        }
+    }
+}
