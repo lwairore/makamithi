438bcb045bcb2c76115ec8ea707bc4015888af43
Lucas Wairore Karangu <kwairore@gmail.com>
2022-03-01 10:35:08 +0300

    - Generate component 'GpGalleryTwoSectionComponent'.

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -4,15 +4,15 @@ import { GalleryRoutingModule } from './gallery-routing.module';
 import { GalleryComponent } from './gallery.component';
 import { SharedModule } from '@sharedModule/shared.module';
 import { GalleryOneMainComponent } from './gallery-one-main/gallery-one-main.component';
-import { GalleryTabOneComponent } from './gallery-one-main/sections/gallery-tab-one/gallery-tab-one.component';
 import { TabsModule } from '../lib/tabs/tabs.module';
+import { GpGalleryTwoSectionComponent } from './gallery-one-main/sections/gp-gallery-two-section/gp-gallery-two-section.component';
 
 
 @NgModule({
   declarations: [
     GalleryComponent,
     GalleryOneMainComponent,
-    GalleryTabOneComponent,
+    GpGalleryTwoSectionComponent,
   ],
   imports: [
     CommonModule,
